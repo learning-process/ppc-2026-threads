f2697b90121d5113ac8420f99690a1a9611f6c03
Vladislav Klimenko <vlad.klimenko.mmv@bk.ru>
2026-02-17 21:40:35 +0300

    upload the first version of the program

---
+++ tasks/klimenko_v_lsh_contrast_incr_seq/common/include/common.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "task/include/task.hpp"
+
+namespace klimenko_v_lsh_contrast_incr_seq {
+
+using InType = std::vector<int>;
+using OutType = std::vector<int>;
+using TestType = int;
+using BaseTask = ppc::task::Task<InType, OutType>;
+
+}  // namespace klimenko_v_lsh_contrast_incr_seq
