b85524dab857a31921795bbf4578c0b2eacf9820
Mamontyonok <kulikartur05@yandex.ru>
2026-02-21 15:14:42 +0300

    tmp2_commit

--- tasks/kulik_a_mat_mul_double_ccs/tests/performance/main.cpp
+++ tasks/kulik_a_mat_mul_double_ccs/tests/performance/main.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <random>
 #include <fstream>
+#include <cmath>
 
 #include "kulik_a_mat_mul_double_ccs/common/include/common.hpp"
 #include "kulik_a_mat_mul_double_ccs/seq/include/ops_seq.hpp"
@@ -55,7 +57,52 @@ class KulikARunPerfTestThreads : public ppc::util::BaseRunPerfTests<InType, OutT
    input_data_ = std::make_tuple(matrix_perf_a, matrix_perf_b);
   }
   bool CheckTestOutputData(OutType &output_data) final {
+    const auto &a = std::get<0>(GetInput());
+    const auto &b = std::get<1>(GetInput());
+    std::vector<double> x(20000);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0.0, 1.0);
+    for (auto& val : vec) {
+        val = dis(gen);
+    }
+    std::vector<double> y(20000, 0.0);
+    for (size_t j = 0; j < b.m; ++j) {
+        double xj = x[j];
+        size_t start = b.col_ind[j];
+        size_t end = b.col_ind[j + 1];
+        for (size_t p = start; p < end; ++p) {
+            size_t i = b.row[p];         
+            y[i] += b.value[p] * xj;
+        }
+    }
+    std::vector<double> res1(20000, 0.0);
+    for (size_t j = 0; j < a.m; ++j) {
+        double yj = y[j];
+        size_t start = a.col_ind[j];
+        size_t end = a.col_ind[j + 1];
+        for (size_t p = start; p < end; ++p) {
+            size_t i = a.row[p];         
+            res1[i] += a.value[p] * yj;
+        }
+    }
+    std::vector<double> res2(20000, 0.0);
+    for (size_t j = 0; j < output_data.m; ++j) {
+        double xj = x[j];
+        size_t start = output_data.col_ind[j];
+        size_t end = output_data.col_ind[j + 1];
+        for (size_t p = start; p < end; ++p) {
+            size_t i = output_data.row[p];         
+            res2[i] += output_data.value[p] * xj;
+        }
+    }
+    for (size_t i = 0; i < 20000; ++i) {
+      if (abs(res1[i] - res2[i]) > 1e-12) {
+        return false;
+      }
+    }
     return true;
+} 
   }
 
   InType GetTestInputData() final {
