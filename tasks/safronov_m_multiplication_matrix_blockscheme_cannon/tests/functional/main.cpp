5d91dc32a1b55dff227d6efa4c4f5f3e7539e87c
SafronovMaks1m <safranov01@list.ru>
2026-02-22 15:39:21 +0300

    clang

--- tasks/safronov_m_multiplication_matrix_blockscheme_cannon/tests/functional/main.cpp
+++ tasks/safronov_m_multiplication_matrix_blockscheme_cannon/tests/functional/main.cpp
@@ -92,7 +92,7 @@ const std::array<TestType, 8> kTestParam = {
 
 const auto kTestTasksList =
     std::tuple_cat(ppc::util::AddFuncTask<SafronovMMultiplicationMatrixBlockSchemeCannon, InType>(
-        kTestParam, PPC_SETTINGS_safronov_m_multiplication_matrix_blockscheme_cannon));//
+        kTestParam, PPC_SETTINGS_safronov_m_multiplication_matrix_blockscheme_cannon));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
