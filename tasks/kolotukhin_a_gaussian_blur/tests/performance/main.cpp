f819df99bec28b555b5346f0808e13db872e1bf2
Alexander Kolotukhin <154746573+Sanchert@users.noreply.github.com>
2026-02-23 00:53:42 +0300

    performance tests

--- tasks/kolotukhin_a_gaussian_blur/tests/performance/main.cpp
+++ tasks/kolotukhin_a_gaussian_blur/tests/performance/main.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <stb/stb_image.h>
 
 #include "kolotukhin_a_gaussian_blur/common/include/common.hpp"
 #include "kolotukhin_a_gaussian_blur/seq/include/ops_seq.hpp"
@@ -7,15 +8,41 @@
 namespace kolotukhin_a_gaussian_blur {
 
 class KolotukhinAGaussinBlurePerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-  const int kCount_ = 200;
   InType input_data_;
+  std::string input_path_;
 
   void SetUp() override {
-    // input_data_ = kCount_;
+    const int rank = ppc::util::GetMPIRank();
+    if (rank != 0) {
+      input_data_ = {std::vector<std::uint8_t>{}, 0, 0};
+      return;
+    }
+    input_path_ = ppc::util::GetAbsoluteTaskPath(PPC_ID_kolotukhin_a_gaussian_blur, "Frog_32x32.png");
+
+    int width = -1;
+    int height = -1;
+    int channels = -1;
+    unsigned char *raw = stbi_load(input_path_.c_str(), &width, &height, &channels, STBI_grey);
+    if (raw == nullptr) {
+      throw std::runtime_error("Load error: " + input_path_);
+    }
+
+    if (width <= 0 || height <= 0) {
+      stbi_image_free(raw);
+      throw std::runtime_error("Image has non-positive dimensions: " + input_path_);
+    }
+
+    const std::size_t img_size = static_cast<std::size_t>(width) * static_cast<std::size_t>(height);
+
+    std::vector<std::uint8_t> data(img_size);
+    std::copy(raw, raw + img_size, data.begin());
+    stbi_image_free(raw);
+
+    input_data_ = {data, width, height};
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return output_data.size() >= 0;
+    return output_data.size() == get<0>(input_data_).size();
   }
 
   InType GetTestInputData() final {
