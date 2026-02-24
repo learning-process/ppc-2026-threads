7f3a249cbb709022aac3bbbf96c4461fcc7a7b4e
AfanasevAlexandr999 <prostosaz2.0@gmail.com>
2026-02-25 01:06:31 +0300

    fix 2

--- tasks/afanasyev_a_integ_rect_method/common/include/common.hpp
+++ tasks/afanasyev_a_integ_rect_method/common/include/common.hpp
@@ -12,4 +12,4 @@ using OutType = double;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
-}  // namespace afanasyev_a_integ_rect_method
+}  // namespace afanasyev_a_integ_rect_method 
