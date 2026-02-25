b343efd16e8d8cb7c5f449bf01b91ae694b43cbe
Ivan Melnik <mel.sirvania@gmail.com>
2026-02-25 21:17:58 +0000

    Tests release

---
+++ tasks/melnik_i_radix_sort_int/tests/functional/main.cpp
@@ -0,0 +1,73 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <random>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "melnik_i_radix_sort_int/seq/include/ops_seq.hpp"
+#include "util/include/func_test_util.hpp"
+#include "util/include/util.hpp"
+
+namespace melnik_i_radix_sort_int {
+
+class MelnikIRadixSortIntFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+ public:
+  static std::string PrintTestParam(const TestType &test_param) {
+    return std::get<1>(test_param);
+  }
+
+ protected:
+  bool CheckTestOutputData(OutType &output_data) final {
+    OutType expected = input_data_;
+    std::sort(expected.begin(), expected.end());
+    return expected == output_data;
+  }
+
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+
+  void SetUp() override {
+    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    input_data_ = std::get<0>(params);
+  }
+
+ private:
+  InType input_data_;
+};
+
+namespace {
+
+TEST_P(MelnikIRadixSortIntFuncTests, SortMatchesStdSort) {
+  ExecuteTest(GetParam());
+}
+
+const std::array<TestType, 9> kTestParam = {
+    std::make_tuple(std::vector<int>{1, 2, 3, 4, 5}, "sorted"),
+    std::make_tuple(std::vector<int>{5, 4, 3, 2, 1}, "reverse_sorted"),
+    std::make_tuple(std::vector<int>{-5, -4, -3, -2, -1}, "reverse_sorted_negative"),
+    std::make_tuple(std::vector<int>{-1, -2, -3, -4, -5}, "sorted_negative"),
+    std::make_tuple(std::vector<int>{3, 1, 4, 1, 5, 9, 2, 6}, "with_duplicates"),
+    std::make_tuple(std::vector<int>{-5, -3, -1, 0, 1, 3, 5}, "mixed"),
+    std::make_tuple(std::vector<int>{-10, -20, -30}, "negative"),
+    std::make_tuple(std::vector<int>{1, 2, 3}, "small_array"),
+    std::make_tuple(std::vector<int>{100, 50, 25, 75, 10}, "random_small"),
+};
+
+const auto kTestTasksList =
+    ppc::util::AddFuncTask<MelnikIRadixSortIntSEQ, InType>(kTestParam, PPC_SETTINGS_melnik_i_radix_sort_int);
+
+const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
+
+const auto kTestName = MelnikIRadixSortIntFuncTests::PrintFuncTestName<MelnikIRadixSortIntFuncTests>;
+
+INSTANTIATE_TEST_SUITE_P(RadixSortTests, MelnikIRadixSortIntFuncTests, kGtestValues, kTestName);
+
+}  // namespace
+
+}  // namespace melnik_i_radix_sort_int
