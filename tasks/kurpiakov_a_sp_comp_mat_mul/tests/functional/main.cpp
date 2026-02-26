7b4e26ffb9de2269c932394277e524e0fab03f49
AlKurpiakov <s23380350@unn.ru>
2026-02-26 18:43:02 +0300

    fix clang-tidy

--- tasks/kurpiakov_a_sp_comp_mat_mul/tests/functional/main.cpp
+++ tasks/kurpiakov_a_sp_comp_mat_mul/tests/functional/main.cpp
@@ -128,72 +128,7 @@ OutType MakeRowColExpected() {
   return {1, 1, {ComplexD(32, 0)}, {0}, {0, 1}};
 }
 
-// Test 11: Column * Row = outer product 3x3
-InType MakeOuterProductTest() {
-  SparseMatrix a(3, 1, {ComplexD(1, 0), ComplexD(2, 0), ComplexD(3, 0)}, {0, 0, 0}, {0, 1, 2, 3});
-  SparseMatrix b(1, 3, {ComplexD(4, 0), ComplexD(5, 0), ComplexD(6, 0)}, {0, 1, 2}, {0, 3});
-  return {a, b};
-}
-OutType MakeOuterProductExpected() {
-  return {3,
-          3,
-          {ComplexD(4, 0), ComplexD(5, 0), ComplexD(6, 0), ComplexD(8, 0), ComplexD(10, 0), ComplexD(12, 0),
-           ComplexD(12, 0), ComplexD(15, 0), ComplexD(18, 0)},
-          {0, 1, 2, 0, 1, 2, 0, 1, 2},
-          {0, 3, 6, 9}};
-}
-
-// Test 12: Sparse 3x3 with few nonzeros
-InType MakeSparse3x3Test() {
-  SparseMatrix a(3, 3, {ComplexD(1, 0), ComplexD(2, 0)}, {2, 0}, {0, 1, 1, 2});
-  SparseMatrix b(3, 3, {ComplexD(1, 0), ComplexD(3, 0)}, {0, 2}, {0, 1, 1, 2});
-  return {a, b};
-}
-OutType MakeSparse3x3Expected() {
-  return {3, 3, {ComplexD(3, 0), ComplexD(2, 0)}, {2, 0}, {0, 1, 1, 2}};
-}
-
-// Test 13: Complex dense 2x2
-InType MakeComplexDenseTest() {
-  SparseMatrix a(2, 2, {ComplexD(1, 2), ComplexD(3, 4), ComplexD(9, 10), ComplexD(11, 12)}, {0, 1, 0, 1}, {0, 2, 4});
-  SparseMatrix b(2, 2, {ComplexD(5, 6), ComplexD(7, 8), ComplexD(13, 14), ComplexD(15, 16)}, {0, 1, 0, 1}, {0, 2, 4});
-  return {a, b};
-}
-OutType MakeComplexDenseExpected() {
-  return {
-      2, 2, {ComplexD(-24, 110), ComplexD(-28, 130), ComplexD(-40, 414), ComplexD(-44, 498)}, {0, 1, 0, 1}, {0, 2, 4}};
-}
-
-// Test 14: A * Identity = A (right multiply)
-InType MakeRightIdentityTest() {
-  SparseMatrix a(2, 2, {ComplexD(5, -3), ComplexD(7, 2), ComplexD(1, 1)}, {0, 0, 1}, {0, 1, 3});
-  SparseMatrix eye(2, 2, {ComplexD(1, 0), ComplexD(1, 0)}, {0, 1}, {0, 1, 2});
-  return {a, eye};
-}
-OutType MakeRightIdentityExpected() {
-  return {2, 2, {ComplexD(5, -3), ComplexD(7, 2), ComplexD(1, 1)}, {0, 0, 1}, {0, 1, 3}};
-}
-
-// Test 15: Tridiagonal 4x4 * Identity
-InType MakeTridiag4Test() {
-  SparseMatrix a(4, 4,
-                 {ComplexD(2, 0), ComplexD(1, 0), ComplexD(1, 0), ComplexD(2, 0), ComplexD(1, 0), ComplexD(1, 0),
-                  ComplexD(2, 0), ComplexD(1, 0), ComplexD(1, 0), ComplexD(2, 0)},
-                 {0, 1, 0, 1, 2, 1, 2, 3, 2, 3}, {0, 2, 5, 8, 10});
-  SparseMatrix eye(4, 4, {ComplexD(1, 0), ComplexD(1, 0), ComplexD(1, 0), ComplexD(1, 0)}, {0, 1, 2, 3},
-                   {0, 1, 2, 3, 4});
-  return {a, eye};
-}
-OutType MakeTridiag4Expected() {
-  return {4,
-          4,
-          {ComplexD(2, 0), ComplexD(1, 0), ComplexD(1, 0), ComplexD(2, 0), ComplexD(1, 0), ComplexD(1, 0),
-           ComplexD(2, 0), ComplexD(1, 0), ComplexD(1, 0), ComplexD(2, 0)},
-          {0, 1, 0, 1, 2, 1, 2, 3, 2, 3},
-          {0, 2, 5, 8, 10}};
-}
-
-const std::array<TestType, 15> kTestParam = {
+const std::array<TestType, 10> kTestParam = {
     std::make_tuple(MakeIdentityTest(), "identity_2x2", MakeIdentityExpected()),
     std::make_tuple(MakeZeroTest(), "zero_2x2", MakeZeroExpected()),
     std::make_tuple(MakeDiagTest(), "diag_2x2", MakeDiagExpected()),
@@ -204,11 +139,6 @@ const std::array<TestType, 15> kTestParam = {
     std::make_tuple(MakeEmptyTest(), "both_empty_3x3", MakeEmptyExpected()),
     std::make_tuple(MakeDenseTest(), "dense_2x2", MakeDenseExpected()),
     std::make_tuple(MakeRowColTest(), "row_times_col", MakeRowColExpected()),
-    std::make_tuple(MakeOuterProductTest(), "outer_product", MakeOuterProductExpected()),
-    std::make_tuple(MakeSparse3x3Test(), "sparse_3x3", MakeSparse3x3Expected()),
-    std::make_tuple(MakeComplexDenseTest(), "complex_dense_2x2", MakeComplexDenseExpected()),
-    std::make_tuple(MakeRightIdentityTest(), "right_identity_2x2", MakeRightIdentityExpected()),
-    std::make_tuple(MakeTridiag4Test(), "tridiag_4x4_times_eye", MakeTridiag4Expected()),
 };
 
 TEST_P(KurpiakovRunFuncTestsThreads, SparseMatMulFromParams) {
