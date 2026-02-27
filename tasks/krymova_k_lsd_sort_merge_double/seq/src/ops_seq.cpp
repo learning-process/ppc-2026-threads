968e707464659e504ef8090476dd088c7d25c16d
Кристина Крымова <kristinakrymova@MacBook-Air-Kristina-2.local>
2026-02-27 21:27:31 +0300

    Add sequential LSD sort with simple merge 8

--- tasks/krymova_k_lsd_sort_merge_double/seq/src/ops_seq.cpp
+++ tasks/krymova_k_lsd_sort_merge_double/seq/src/ops_seq.cpp
@@ -4,7 +4,7 @@
 #include <cstdint>
 #include <cstring>
 #include <vector>
-
+#include"krymova_k_lsd_sort_merge_double/common/include/common.hpp"
 namespace krymova_k_lsd_sort_merge_double {
 
 KrymovaKLsdSortMergeDoubleSEQ::KrymovaKLsdSortMergeDoubleSEQ(const InType &in) {
