8b8b4b4a2b01caf494028f37478a2f283d2e3976
Gr1gro <safaryangrigor6564@mail.ru>
2026-02-27 22:47:00 +0300

    push

---
+++ tasks/safaryan_a_sparse_matrix_mult_crs_seq/seq/include/ops_seq.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "safaryan_a_sparse_matrix_mult_crs_seq/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace safaryan_a_sparse_matrix_mult_crs_seq {
+
+class SafaryanASparseMatrixMultCRSSeq : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSEQ;
+  }
+
+  explicit SafaryanASparseMatrixMultCRSSeq(const InType& in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  static bool IsMatrixValid(const CRSMatrix& m);
+};
+
+}  // namespace safaryan_a_sparse_matrix_mult_crs_seq
