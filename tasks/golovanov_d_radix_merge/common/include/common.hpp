3243603bbe0009bf849e0200a99c76c54d2a4053
DtheMakeeeR <danilgol45@mail.ru>
2026-02-27 13:41:00 +0300

    common

--- tasks/golovanov_d_radix_merge/common/include/common.hpp
+++ tasks/golovanov_d_radix_merge/common/include/common.hpp
@@ -1,15 +1,15 @@
 #pragma once
 
 #include <string>
-#include <tuple>
+#include <vector>
 
 #include "task/include/task.hpp"
 
 namespace golovanov_d_radix_merge {
 
-using InType = int;
-using OutType = int;
-using TestType = std::tuple<int, std::string>;
+using InType = std::vector<double>;
+using OutType = std::vector<double>;
+using TestType = std::size_t;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace golovanov_d_radix_merge
