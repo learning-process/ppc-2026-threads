0db0629512c4d7403ef445bcc9e5d525bd79751c
Offsidetax <dspichek@bk.ru>
2026-02-27 19:33:19 +0300

    Fixed clang-tidy

--- tasks/spichek_d_radix_sort_for_integers_with_simple_merging/tests/performance/main.cpp
+++ tasks/spichek_d_radix_sort_for_integers_with_simple_merging/tests/performance/main.cpp
@@ -5,11 +5,13 @@
 
 #include "spichek_d_radix_sort_for_integers_with_simple_merging/common/include/common.hpp"
 #include "spichek_d_radix_sort_for_integers_with_simple_merging/seq/include/ops_seq.hpp"
-#include "task/include/task.hpp"  // Добавлено для ppc::task::TypeOfTask
+#include "task/include/task.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace spichek_d_radix_sort_for_integers_with_simple_merging {
 
+// Создаем заглушки для других типов задач, наследуясь от последовательной версии.
+// Это необходимо, чтобы MakeAllPerfTasks мог создать уникальные имена тестов.
 class RadixSortOMP : public RadixSortSEQ {
  public:
   using RadixSortSEQ::RadixSortSEQ;
@@ -44,23 +46,23 @@ class RadixSortALL : public RadixSortSEQ {
 
 class RadixSortRunPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
-  // Исправлено именование согласно readability-identifier-naming
-  const int kCount = 1000000;
-  InType input_data;  // Удалена избыточная инициализация {}
+  // Исправлено именование согласно правилу readability-identifier-naming
+  const int k_count = 1000000;
+  InType input_data;
 
   void SetUp() override {
-    input_data.resize(kCount);
+    input_data.resize(k_count);
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dist(-100000, 100000);
 
-    for (int i = 0; i < kCount; ++i) {
+    for (int i = 0; i < k_count; ++i) {
       input_data[i] = dist(gen);
     }
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    return std::ranges::is_sorted(output_data);  // Переход на ranges
+    return std::ranges::is_sorted(output_data);
   }
 
   InType GetTestInputData() final {
@@ -74,11 +76,13 @@ TEST_P(RadixSortRunPerfTest, RunPerfModes) {
 
 namespace {
 
+// Используем MakeAllPerfTasks точно так же, как в образце.
 const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, RadixSortALL, RadixSortOMP, RadixSortSEQ, RadixSortSTL, RadixSortTBB>(
         PPC_SETTINGS_spichek_d_radix_sort_for_integers_with_simple_merging);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
 const auto kPerfTestName = RadixSortRunPerfTest::CustomPerfTestName;
 
 INSTANTIATE_TEST_SUITE_P(RunModeTests, RadixSortRunPerfTest, kGtestValues, kPerfTestName);
