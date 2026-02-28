408d5f46fd38efb2fac3f95268d0d1df69f83495
MishaRysev <r0botbob@yandex.ru>
2026-02-28 19:23:12 +0000

    123

--- tasks/rysev_m_linear_filter_gauss_kernel/tests/functional/main.cpp
+++ tasks/rysev_m_linear_filter_gauss_kernel/tests/functional/main.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <stb/stb_image.h>
 
-#include <algorithm>  // abc
+#include <algorithm> 
 #include <array>
 #include <cstddef>
 #include <cstdint>
@@ -12,12 +12,8 @@
 #include <utility>
 #include <vector>
 
-#include "rysev_m_linear_filter_gauss_kernel/all/include/ops_all.hpp"
 #include "rysev_m_linear_filter_gauss_kernel/common/include/common.hpp"
-#include "rysev_m_linear_filter_gauss_kernel/omp/include/ops_omp.hpp"
 #include "rysev_m_linear_filter_gauss_kernel/seq/include/ops_seq.hpp"
-#include "rysev_m_linear_filter_gauss_kernel/stl/include/ops_stl.hpp"
-#include "rysev_m_linear_filter_gauss_kernel/tbb/include/ops_tbb.hpp"
 #include "util/include/func_test_util.hpp"
 #include "util/include/util.hpp"
 
