0884cda09237bdaef68dbca2db58e1bb83721b4e
Leonova Anna <leonovaa441@gmail.com>
2026-02-28 14:58:12 +0000

    qq

---
+++ tasks/leonova_a_radix_merge_sort/common/include/common.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+#include "task/include/task.hpp"
+
+namespace leonova_a_radix_merge_sort {
+
+using InType = std::vector<int64_t>;
+using OutType = std::vector<int64_t>;
+using TestType = std::tuple<std::vector<int64_t>, std::vector<int64_t>>;
+using BaseTask = ppc::task::Task<InType, OutType>;
+
+}  // namespace leonova_a_radix_merge_sort
