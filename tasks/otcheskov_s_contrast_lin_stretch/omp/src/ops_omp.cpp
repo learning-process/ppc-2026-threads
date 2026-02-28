fc88bdfbbc7ea5cdde98ff241b5150658f71fd62
Serebro1 <OtcheskovSemen@yandex.ru>
2026-02-28 20:06:16 +0300

    clang-tidy (2)

--- tasks/otcheskov_s_contrast_lin_stretch/omp/src/ops_omp.cpp
+++ tasks/otcheskov_s_contrast_lin_stretch/omp/src/ops_omp.cpp
@@ -31,20 +31,21 @@ bool OtcheskovSContrastLinStretchOMP::RunImpl() {
 
   const InType &input = GetInput();
   OutType &output = GetOutput();
+  const size_t size = input.size();
 
   uint8_t min_val = 255;
   uint8_t max_val = 0;
 
-#pragma omp parallel for default(none) shared(input) reduction(min : min_val) reduction(max : max_val)
-  for (size_t i = 0; i < input.size(); ++i) {
+#pragma omp parallel for default(none) shared(input, size) reduction(min : min_val) reduction(max : max_val)
+  for (size_t i = 0; i < size; ++i) {
     min_val = std::min(input[i], min_val);
     max_val = std::max(input[i], max_val);
   }
 
   const size_t threshold_size = 1000000;
   if (min_val == max_val) {
-#pragma omp parallel for if (input.size() > threshold_size) default(none) shared(input, output)
-    for (size_t i = 0; i < input.size(); ++i) {
+#pragma omp parallel for if (input.size() > threshold_size) default(none) shared(input, output, size)
+    for (size_t i = 0; i < size; ++i) {
       output[i] = input[i];
     }
 
@@ -54,8 +55,8 @@ bool OtcheskovSContrastLinStretchOMP::RunImpl() {
   const int min_i = static_cast<int>(min_val);
   const int range = static_cast<int>(max_val) - min_i;
 
-#pragma omp parallel for default(none) shared(input, output, min_i, range)
-  for (size_t i = 0; i < input.size(); ++i) {
+#pragma omp parallel for default(none) shared(input, output, min_i, range, size)
+  for (size_t i = 0; i < size; ++i) {
     int pixel = static_cast<int>(input[i]);
     int value = (pixel - min_i) * 255 / range;
     output[i] = static_cast<uint8_t>(std::clamp(value, 0, 255));
