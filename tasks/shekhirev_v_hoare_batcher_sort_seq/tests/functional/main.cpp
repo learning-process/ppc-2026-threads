7206fa4b4c332f6d0052a32d2d42e44ee747e5aa
root <gamarfastrast@gmail.com>
2026-02-28 16:33:47 +0000

    covreport fix

--- tasks/shekhirev_v_hoare_batcher_sort_seq/tests/functional/main.cpp
+++ tasks/shekhirev_v_hoare_batcher_sort_seq/tests/functional/main.cpp
@@ -16,6 +16,12 @@ namespace shekhirev_v_hoare_batcher_sort_seq {
 using TaskTestType = std::tuple<size_t, int>;
 
 class ShekhirevVFuncTest : public ppc::util::BaseRunFuncTests<InType, OutType, TaskTestType> {
+ public:
+  static std::string PrintTestParam(const TaskTestType &test_param) {
+    auto [size, seed] = test_param;
+    return "Size_" + std::to_string(size) + "_Seed_" + std::to_string(seed);
+  }
+
  protected:
   void SetUp() override {
     auto [size, seed] = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
@@ -46,7 +52,6 @@ TEST_P(ShekhirevVFuncTest, SeqSortTests) {
 }
 
 namespace {
-
 const std::array<TaskTestType, 6> kTestParams = {std::make_tuple(0, 42),  std::make_tuple(1, 42),
                                                  std::make_tuple(8, 7),   std::make_tuple(13, 13),
                                                  std::make_tuple(128, 1), std::make_tuple(200, 123)};
@@ -58,7 +63,6 @@ const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
 INSTANTIATE_TEST_SUITE_P(SeqSortTests_Group, ShekhirevVFuncTest, kGtestValues,
                          ShekhirevVFuncTest::PrintFuncTestName<ShekhirevVFuncTest>);
-
 }  // namespace
 
 }  // namespace shekhirev_v_hoare_batcher_sort_seq
