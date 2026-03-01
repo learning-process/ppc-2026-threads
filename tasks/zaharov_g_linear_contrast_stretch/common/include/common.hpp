1d39f8471c82f976b62677ea661c664b8a143872
Sweetbread <risdeveau@lair.moe>
2026-03-01 19:50:23 +0300

    SEQ

--- tasks/zaharov_g_linear_contrast_stretch/common/include/common.hpp
+++ tasks/zaharov_g_linear_contrast_stretch/common/include/common.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 #include <tuple>
+#include <vector>
 
 #include "task/include/task.hpp"
 
 namespace zaharov_g_linear_contrast_stretch {
 
-using InType = int;
-using OutType = int;
+using InType = std::vector<uint8_t>;
+using OutType = std::vector<uint8_t>;
 using TestType = std::tuple<int, std::string>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
