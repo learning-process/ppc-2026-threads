163fbf1880567401f5b72de3bb2b005c3294e246
Mariya <MariyShelenkova@yandex.ru>
2026-03-01 21:07:08 +0300

    shelenkova

---
+++ tasks/shelenkova_m_shell_sort_simple_merge/seq/include/ops_seq.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "shelenkova_m_shell_sort_simple_merge/common/include/common.hpp"
+
+namespace shelenkova_m_shell_sort_simple_merge {
+
+class ShelenkovaMShellSortSimpleMergeSEQ : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSEQ;
+  }
+  explicit ShelenkovaMShellSortSimpleMergeSEQ(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace shelenkova_m_shell_sort_simple_merge
