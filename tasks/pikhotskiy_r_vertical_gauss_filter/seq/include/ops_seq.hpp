97e966fb4ec11b3e7cd56c37d504053873a0e105
roman-pikhotskiy <romapanda5@mail.ru>
2026-03-01 21:45:13 +0300

    add_files

---
+++ tasks/pikhotskiy_r_vertical_gauss_filter/seq/include/ops_seq.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "pikhotskiy_r_vertical_gauss_filter/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace pikhotskiy_r_vertical_gauss_filter {
+
+class PikhotskiyRVerticalGaussFilterSEQ : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSEQ;
+  }
+  explicit PikhotskiyRVerticalGaussFilterSEQ(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace pikhotskiy_r_vertical_gauss_filter
