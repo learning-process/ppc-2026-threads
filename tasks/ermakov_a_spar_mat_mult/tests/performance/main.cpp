6d14c5379c764fc47589889a9f2abcc4dab21297
Ermakov Alexei <lioshaermakov32@gmail.com>
2026-03-04 11:02:32 +0300

    15000 for perf tests

--- tasks/ermakov_a_spar_mat_mult/tests/performance/main.cpp
+++ tasks/ermakov_a_spar_mat_mult/tests/performance/main.cpp
@@ -62,7 +62,7 @@ MatrixCRS DenseToCRS(const DenseMatrix &m, double eps = 1e-12) {
 
 class ErmakovARunPerfTest : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
-  const int k_count = 20000;
+  const int k_count = 15000;
   InType input_data{};
 
   void SetUp() override {
