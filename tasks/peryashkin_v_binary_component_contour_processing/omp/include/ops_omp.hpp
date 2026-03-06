bc1906a3b0e5b4d278344c8fed3eec658f0e4fa5
Excite <vasyaperyashkin223@gmail.com>
2026-03-06 13:43:57 +0000

    fix clang tidy

--- tasks/peryashkin_v_binary_component_contour_processing/omp/include/ops_omp.hpp
+++ tasks/peryashkin_v_binary_component_contour_processing/omp/include/ops_omp.hpp
@@ -1,6 +1,8 @@
+// tasks/peryashkin_v_binary_component_contour_processing/omp/include/ops_omp.hpp
 #pragma once
 
 #include "peryashkin_v_binary_component_contour_processing/common/include/common.hpp"
+#include "task/include/task.hpp"
 
 namespace peryashkin_v_binary_component_contour_processing {
 
@@ -9,8 +11,7 @@ class PeryashkinVBinaryComponentContourProcessingOMP : public BaseTask {
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
     return ppc::task::TypeOfTask::kOMP;
   }
-
-  explicit PeryashkinVBinaryComponentContourProcessingOMP(const InType &in);
+  explicit PeryashkinVBinaryComponentContourProcessingOMP(const InType& in);
 
  private:
   bool ValidationImpl() override;
