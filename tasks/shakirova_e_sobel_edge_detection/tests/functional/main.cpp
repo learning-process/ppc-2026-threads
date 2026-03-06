4b8add795c30f7cc197c5d2d072226b742ba71d7
Eseniya Shakirova <shakirova.esya23@mail.ru>
2026-03-06 17:31:42 +0300

    Шакирова Есения. Технология SEQ. Выделение ребер на изображении с использованием оператора Собеля. Вариант 27 (#103)
    * laba is ready
    
    * laba is ready
    
    * laba is ready
    
    * laba is ready
    
    * laba is ready
    
    * laba is ready

--- tasks/shakirova_e_sobel_edge_detection/tests/functional/main.cpp
+++ tasks/shakirova_e_sobel_edge_detection/tests/functional/main.cpp
@@ -72,12 +72,13 @@ const std::array<TestType, 6> kTestParam = {
     std::make_tuple(0, "test_4.txt"), std::make_tuple(2, "test_5.txt"),  std::make_tuple(2, "test_6.txt"),
 };
 
-const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<ShakirovaESobelEdgeDetectionSEQ, InType>(
-                                               kTestParam, PPC_SETTINGS_shakirova_e_sobel_edge_detection),
-                                           ppc::util::AddFuncTask<ShakirovaESobelEdgeDetectionOMP, InType>(
-                                               kTestParam, PPC_SETTINGS_shakirova_e_sobel_edge_detection),
-                                           ppc::util::AddFuncTask<ShakirovaESobelEdgeDetectionALL, InType>(
-                                               kTestParam, PPC_SETTINGS_shakirova_e_sobel_edge_detection));
+const auto kTestTasksList = std::tuple_cat(
+    ppc::util::AddFuncTask<ShakirovaESobelEdgeDetectionSEQ, InType>(
+        kTestParam, PPC_SETTINGS_shakirova_e_sobel_edge_detection),
+    ppc::util::AddFuncTask<ShakirovaESobelEdgeDetectionOMP, InType>(
+        kTestParam, PPC_SETTINGS_shakirova_e_sobel_edge_detection),
+    ppc::util::AddFuncTask<ShakirovaESobelEdgeDetectionALL, InType>(
+        kTestParam, PPC_SETTINGS_shakirova_e_sobel_edge_detection));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
