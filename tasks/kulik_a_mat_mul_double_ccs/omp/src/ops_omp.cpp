635fbc39ddfc9fc345bdbf462ae92620dd222324
Mamontyonok <kulikartur05@yandex.ru>
2026-03-06 09:48:05 +0300

    clang-format

--- tasks/kulik_a_mat_mul_double_ccs/omp/src/ops_omp.cpp
+++ tasks/kulik_a_mat_mul_double_ccs/omp/src/ops_omp.cpp
@@ -37,13 +37,13 @@ bool KulikAMatMulDoubleCcsOMP::RunImpl() {
   std::vector<std::vector<double>> local_values(b.m);
   std::vector<std::vector<size_t>> local_rows(b.m);
 
-  int num_threads = omp_get_max_threads(); 
-  
+  int num_threads = omp_get_max_threads();
+
   std::vector<std::vector<double>> thread_accum(num_threads, std::vector<double>(a.n, 0.0));
   std::vector<std::vector<bool>> thread_nz(num_threads, std::vector<bool>(a.n, false));
   std::vector<std::vector<size_t>> thread_nnz_rows(num_threads);
 
-  #pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(static)
   for (int j = 0; j < static_cast<int>(b.m); ++j) {
     int tid = omp_get_thread_num();
     for (size_t k = b.col_ind[j]; k < b.col_ind[j + 1]; ++k) {
@@ -51,7 +51,7 @@ bool KulikAMatMulDoubleCcsOMP::RunImpl() {
       double b_val = b.value[k];
       for (size_t zc = a.col_ind[ind]; zc < a.col_ind[ind + 1]; ++zc) {
         size_t i = a.row[zc];
-        double a_val = a.value[zc];       
+        double a_val = a.value[zc];
         thread_accum[tid][i] += a_val * b_val;
         if (!thread_nz[tid][i]) {
           thread_nz[tid][i] = true;
@@ -59,9 +59,9 @@ bool KulikAMatMulDoubleCcsOMP::RunImpl() {
         }
       }
     }
-    
+
     std::ranges::sort(thread_nnz_rows[tid]);
-    
+
     for (size_t i : thread_nnz_rows[tid]) {
       if (thread_accum[tid][i] != 0.0) {
         local_rows[j].push_back(i);
@@ -84,7 +84,7 @@ bool KulikAMatMulDoubleCcsOMP::RunImpl() {
   c.value.resize(total_nz);
   c.row.resize(total_nz);
 
-  #pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(static)
   for (int j = 0; j < static_cast<int>(b.m); ++j) {
     size_t offset = c.col_ind[j];
     size_t col_nz = local_values[j].size();
