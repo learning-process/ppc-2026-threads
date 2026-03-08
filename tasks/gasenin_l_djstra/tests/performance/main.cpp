05204b527c5f870fa5683dd9d89af8e51e5325e0
Leontin16 <gasenidze@mail.ru>
2026-03-08 17:36:29 +0300

    patch 1.21.3.1.0.0.11

--- tasks/gasenin_l_djstra/tests/performance/main.cpp
+++ tasks/gasenin_l_djstra/tests/performance/main.cpp
@@ -2,6 +2,7 @@
 
 #include "gasenin_l_djstra/common/include/common.hpp"
 #include "gasenin_l_djstra/seq/include/ops_seq.hpp"
+#include "gasenin_l_djstra/omp/include/ops_omp.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace gasenin_l_djstra {
@@ -30,7 +31,9 @@ TEST_P(DjkstraPerfTests, RunPerfModes) {
 
 namespace {
 
-const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, GaseninLDjstraSEQ>(PPC_SETTINGS_gasenin_l_djstra);
+const auto kSeqPerfTasks = ppc::util::MakeAllPerfTasks<InType, GaseninLDjstraSEQ>(PPC_SETTINGS_gasenin_l_djstra);
+const auto kOmpPerfTasks = ppc::util::MakeAllPerfTasks<InType, GaseninLDjstraOMP>(PPC_SETTINGS_gasenin_l_djstra);
+const auto kAllPerfTasks = std::tuple_cat(kSeqPerfTasks, kOmpPerfTasks);
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 const auto kPerfTestName = DjkstraPerfTests::CustomPerfTestName;
 
