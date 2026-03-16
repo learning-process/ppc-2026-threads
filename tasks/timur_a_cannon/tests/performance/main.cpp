25db9c250d557f0355084a377470d238b4ca7038
TIMUR <timur.sinev2000@mail.ru>
2026-03-16 22:11:36 +0300

    omp8

--- tasks/timur_a_cannon/tests/performance/main.cpp
+++ tasks/timur_a_cannon/tests/performance/main.cpp
@@ -13,18 +13,16 @@
 namespace timur_a_cannon {
 
 class TimurACannonPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
- protected: 
+ protected:
   InType input_data_;
 
-
   void SetUp() override {
-    const int kCount = 512; 
+    const int kCount = 512;
     int size_block = 32;
     std::vector<std::vector<double>> matrix_a(kCount, std::vector<double>(kCount, 2.0));
     std::vector<std::vector<double>> matrix_b(kCount, std::vector<double>(kCount, 3.0));
 
     input_data_ = std::make_tuple(size_block, matrix_a, matrix_b);
-
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
