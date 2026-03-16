b662fd4e10853d2c99a76fc820363766a7edb0e0
TIMUR <timur.sinev2000@mail.ru>
2026-03-16 21:26:00 +0300

    omp3

--- tasks/timur_a_cannon/tests/performance/main.cpp
+++ tasks/timur_a_cannon/tests/performance/main.cpp
@@ -55,10 +55,10 @@ const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, TimurACannonMatrixMultiplication, TimurACannonMatrixMultiplicationOMP>(
         PPC_SETTINGS_timur_a_cannon);
 
-const auto kPerfValues = ppc::util::ExpandToValues(kAllPerfTasks);
+        
 const auto kPerfTestName = TimurACannonPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, TimurACannonPerfTests, kPerfValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, TimurACannonPerfTests, kAllPerfTasks, kPerfTestName);
 
 }  // namespace
 
