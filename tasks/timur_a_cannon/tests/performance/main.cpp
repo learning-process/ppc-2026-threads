a7c657de6f9ff538df29f122c6a4b225ee225193
TIMUR <timur.sinev2000@mail.ru>
2026-03-16 21:46:03 +0300

    omp1

--- tasks/timur_a_cannon/tests/performance/main.cpp
+++ tasks/timur_a_cannon/tests/performance/main.cpp
@@ -51,17 +51,15 @@ TEST_P(TimurACannonPerfTests, MultiplicationMatrixBlockSchemeCannonPerf) {
 
 namespace {
 
-const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType,
-                                                       TimurACannonMatrixMultiplication,
-                                                       TimurACannonMatrixMultiplicationOMP>(
-    PPC_SETTINGS_timur_a_cannon);
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, TimurACannonMatrixMultiplication, TimurACannonMatrixMultiplicationOMP>(
+        PPC_SETTINGS_timur_a_cannon);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
 const auto kPerfTestName = TimurACannonMatrixMultiplicationPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, TimurACannonMatrixMultiplicationPerfTests, kGtestValues,
-                         kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, TimurACannonMatrixMultiplicationPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace
 
