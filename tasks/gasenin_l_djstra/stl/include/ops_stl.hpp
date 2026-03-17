c0255ab9181fcd2fa0109ce9ae8ebf0eaa588e5c
Leontin16 <gasenidze@mail.ru>
2026-03-17 19:56:47 +0300

    patch 1.21.2.0.0.0.3

--- tasks/gasenin_l_djstra/stl/include/ops_stl.hpp
+++ tasks/gasenin_l_djstra/stl/include/ops_stl.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <condition_variable>
+#include <mutex>
+#include <thread>
 #include <vector>
 
 #include "gasenin_l_djstra/common/include/common.hpp"
@@ -20,8 +23,31 @@ class GaseninLDjstraSTL : public BaseTask {
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
+  enum class Phase {
+    kIdle,
+    kFindMin,
+    kRelax,
+    kStop,
+  };
+
+  void WorkerLoop(int thread_id);
+  void Dispatch(Phase phase);
+
   std::vector<InType> dist_;
   std::vector<char> visited_;
+  std::vector<InType> local_min_;
+  std::vector<InType> local_vert_;
+  InType global_vertex_ = -1;
+  int num_threads_ = 0;
+
+  std::mutex mtx_;
+  std::condition_variable cv_start_;
+  std::condition_variable cv_done_;
+  Phase phase_ = Phase::kIdle;
+  int generation_ = 0;
+  int pending_ = 0;
+
+  std::vector<std::thread> workers_;
 };
 
 }  // namespace gasenin_l_djstra
--- tasks/gasenin_l_djstra/stl/src/ops_stl.cpp
+++ tasks/gasenin_l_djstra/stl/src/ops_stl.cpp
@@ -1,62 +1,18 @@
 #include "gasenin_l_djstra/stl/include/ops_stl.hpp"
 
 #include <algorithm>
-#include <barrier>
 #include <cstdint>
 #include <cstdlib>
 #include <limits>
+#include <mutex>
 #include <thread>
+#include <utility>
 #include <vector>
 
 #include "gasenin_l_djstra/common/include/common.hpp"
 #include "util/include/util.hpp"
 
 namespace gasenin_l_djstra {
-
-namespace {
-
-void ThreadFindMin(int thread_id, int num_threads, InType n, InType inf, const std::vector<InType> &dist,
-                   const std::vector<char> &visited, std::vector<InType> &local_min, std::vector<InType> &local_vert) {
-  InType t_min = inf;
-  InType t_vert = -1;
-  for (int idx = thread_id; idx < n; idx += num_threads) {
-    if (visited[idx] == 0 && dist[idx] < t_min) {
-      t_min = dist[idx];
-      t_vert = idx;
-    }
-  }
-  local_min[thread_id] = t_min;
-  local_vert[thread_id] = t_vert;
-}
-
-void ThreadRelaxEdges(int thread_id, int num_threads, InType n, InType inf, InType global_vertex,
-                      std::vector<InType> &dist, const std::vector<char> &visited) {
-  for (int vertex = thread_id; vertex < n; vertex += num_threads) {
-    if (visited[vertex] == 0 && vertex != global_vertex && dist[global_vertex] != inf) {
-      dist[vertex] = std::min(dist[vertex], dist[global_vertex] + std::abs(global_vertex - vertex));
-    }
-  }
-}
-
-InType ReduceAndMark(int num_threads, InType inf, std::vector<InType> &local_min, std::vector<InType> &local_vert,
-                     std::vector<char> &visited) {
-  InType global_min = inf;
-  InType global_vertex = -1;
-  for (int ti = 0; ti < num_threads; ++ti) {
-    if (local_min[ti] < global_min) {
-      global_min = local_min[ti];
-      global_vertex = local_vert[ti];
-    }
-  }
-  if (global_vertex != -1 && global_min != inf) {
-    visited[global_vertex] = 1;
-  } else {
-    global_vertex = -1;
-  }
-  return global_vertex;
-}
-
-}  // namespace
 
 GaseninLDjstraSTL::GaseninLDjstraSTL(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
@@ -74,47 +30,104 @@ bool GaseninLDjstraSTL::PreProcessingImpl() {
 
   dist_.assign(n, inf);
   visited_.assign(n, 0);
-
   dist_[0] = 0;
+
+  num_threads_ = ppc::util::GetNumThreads();
+  local_min_.assign(num_threads_, inf);
+  local_vert_.assign(num_threads_, -1);
+
+  workers_.resize(num_threads_);
+  for (int thread_id = 0; thread_id < num_threads_; ++thread_id) {
+    workers_[thread_id] = std::thread(&GaseninLDjstraSTL::WorkerLoop, this, thread_id);
+  }
+
   return true;
+}
+
+void GaseninLDjstraSTL::WorkerLoop(int thread_id) {
+  const InType n = GetInput();
+  const InType inf = std::numeric_limits<InType>::max();
+  int my_gen = 0;
+
+  while (true) {
+    Phase current_phase{};
+    {
+      std::unique_lock<std::mutex> lock(mtx_);
+      cv_start_.wait(lock, [&] { return generation_ > my_gen; });
+      my_gen = generation_;
+      current_phase = phase_;
+    }
+
+    if (current_phase == Phase::kStop) {
+      return;
+    }
+
+    if (current_phase == Phase::kFindMin) {
+      InType thread_min = inf;
+      InType thread_vert = -1;
+      for (int idx = thread_id; idx < n; idx += num_threads_) {
+        if (visited_[idx] == 0 && dist_[idx] < thread_min) {
+          thread_min = dist_[idx];
+          thread_vert = idx;
+        }
+      }
+      local_min_[thread_id] = thread_min;
+      local_vert_[thread_id] = thread_vert;
+    } else {
+      const InType src = global_vertex_;
+      for (int vertex = thread_id; vertex < n; vertex += num_threads_) {
+        if (visited_[vertex] == 0 && vertex != src && dist_[src] != inf) {
+          dist_[vertex] = std::min(dist_[vertex], dist_[src] + std::abs(src - vertex));
+        }
+      }
+    }
+
+    {
+      std::lock_guard<std::mutex> lock(mtx_);
+      if (--pending_ == 0) {
+        cv_done_.notify_one();
+      }
+    }
+  }
+}
+
+void GaseninLDjstraSTL::Dispatch(Phase phase) {
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    phase_ = phase;
+    pending_ = num_threads_;
+    ++generation_;
+    cv_start_.notify_all();
+  }
+  std::unique_lock<std::mutex> lock(mtx_);
+  cv_done_.wait(lock, [&] { return pending_ == 0; });
 }
 
 bool GaseninLDjstraSTL::RunImpl() {
   const InType n = GetInput();
   const InType inf = std::numeric_limits<InType>::max();
-  const int num_threads = ppc::util::GetNumThreads();
 
-  std::vector<InType> local_min(num_threads, inf);
-  std::vector<InType> local_vert(num_threads, -1);
-  InType global_vertex = -1;
+  for (int iter = 0; iter < n; ++iter) {
+    Dispatch(Phase::kFindMin);
 
-  auto on_find_sync = [&]() noexcept {
-    global_vertex = ReduceAndMark(num_threads, inf, local_min, local_vert, visited_);
-    std::fill(local_min.begin(), local_min.end(), inf);
-    std::fill(local_vert.begin(), local_vert.end(), -1);
-  };
-
-  std::barrier<decltype(on_find_sync)> bar_find(num_threads, std::move(on_find_sync));
-  std::barrier<> bar_relax(num_threads);
-
-  auto worker = [&](int thread_id) {
-    for (int iter = 0; iter < n; ++iter) {
-      ThreadFindMin(thread_id, num_threads, n, inf, dist_, visited_, local_min, local_vert);
-      bar_find.arrive_and_wait();
-      if (global_vertex == -1) {
-        break;
+    InType global_min = inf;
+    global_vertex_ = -1;
+    for (int thread_idx = 0; thread_idx < num_threads_; ++thread_idx) {
+      if (local_min_[thread_idx] < global_min) {
+        global_min = local_min_[thread_idx];
+        global_vertex_ = local_vert_[thread_idx];
       }
-      ThreadRelaxEdges(thread_id, num_threads, n, inf, global_vertex, dist_, visited_);
-      bar_relax.arrive_and_wait();
     }
-  };
 
-  std::vector<std::thread> threads(num_threads);
-  for (int thread_id = 0; thread_id < num_threads; ++thread_id) {
-    threads[thread_id] = std::thread(worker, thread_id);
-  }
-  for (auto &th : threads) {
-    th.join();
+    if (global_vertex_ == -1 || global_min == inf) {
+      break;
+    }
+
+    visited_[global_vertex_] = 1;
+    std::ranges::fill(local_min_, inf);
+    std::ranges::fill(local_vert_, -1);
+
+    Dispatch(Phase::kRelax);
   }
 
   int64_t total_sum = 0;
@@ -128,6 +141,16 @@ bool GaseninLDjstraSTL::RunImpl() {
 }
 
 bool GaseninLDjstraSTL::PostProcessingImpl() {
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    phase_ = Phase::kStop;
+    ++generation_;
+    cv_start_.notify_all();
+  }
+  for (auto &worker : workers_) {
+    worker.join();
+  }
+  workers_.clear();
   return true;
 }
 
