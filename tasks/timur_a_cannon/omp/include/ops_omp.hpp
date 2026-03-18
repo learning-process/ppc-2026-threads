486729b1795a819930a501299ea96298608b8e77
TIMUR <timur.sinev2000@mail.ru>
2026-03-18 03:24:54 +0300

    ompF88

--- tasks/timur_a_cannon/omp/include/ops_omp.hpp
+++ tasks/timur_a_cannon/omp/include/ops_omp.hpp
@@ -22,19 +22,17 @@ class TimurACannonMatrixMultiplicationOMP : public BaseTask {
   bool RunImpl() override;
   bool PostProcessingImpl() override;
 
-  static void RotateBlocksA(std::vector<std::vector<std::vector<std::vector<double>>>> &blocks, int grid_sz);
-  static void RotateBlocksB(std::vector<std::vector<std::vector<std::vector<double>>>> &blocks, int grid_sz);
-  static void BlockMultiplyAccumulate(const std::vector<std::vector<double>> &a,
-                                      const std::vector<std::vector<double>> &b, std::vector<std::vector<double>> &c,
-                                      int b_size);
+  void RotateBlocksA(std::vector<std::vector<std::vector<std::vector<double>>>> &blocks, int grid_sz);
+  void RotateBlocksB(std::vector<std::vector<std::vector<std::vector<double>>>> &blocks, int grid_sz);
+  void BlockMultiplyAccumulate(const std::vector<std::vector<double>> &a, const std::vector<std::vector<double>> &b,
+                               std::vector<std::vector<double>> &c, int b_size);
 
-  static void DistributeData(const std::vector<std::vector<double>> &src_a,
-                             const std::vector<std::vector<double>> &src_b,
-                             std::vector<std::vector<std::vector<std::vector<double>>>> &bl_a,
-                             std::vector<std::vector<std::vector<std::vector<double>>>> &bl_b, int b_size, int grid_sz);
+  void DistributeData(const std::vector<std::vector<double>> &src_a, const std::vector<std::vector<double>> &src_b,
+                      std::vector<std::vector<std::vector<std::vector<double>>>> &bl_a,
+                      std::vector<std::vector<std::vector<std::vector<double>>>> &bl_b, int b_size, int grid_sz);
 
-  static void CollectResult(const std::vector<std::vector<std::vector<std::vector<double>>>> &bl_c,
-                            std::vector<std::vector<double>> &res, int b_size, int grid_sz);
+  void CollectResult(const std::vector<std::vector<std::vector<std::vector<double>>>> &bl_c,
+                     std::vector<std::vector<double>> &res, int b_size, int grid_sz);
 };
 
 }  // namespace timur_a_cannon
