34a5e1a0f26c0f7192e3efd8b908c5475b17da62
Makasimchik <titaevmaks2005@gmail.com>
2026-03-21 20:48:35 +0300

    ver1

---
+++ tasks/titaev_m_sortirovka_betchera/omp/include/ops_omp.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+#include "task/include/task.hpp"
+#include "titaev_m_sortirovka_betchera/common/include/common.hpp"
+
+namespace titaev_m_sortirovka_betchera {
+
+class TitaevSortirovkaBetcheraOMP : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kOMP;
+  }
+  explicit TitaevSortirovkaBetcheraOMP(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  static void ConvertToKeys(const InType &input, std::vector<uint64_t> &keys);
+  static void RadixSort(std::vector<uint64_t> &keys);
+  static void ConvertFromKeys(const std::vector<uint64_t> &keys, OutType &output);
+  void BatcherSort();
+
+  static void BatcherStep(OutType &result, size_t n, size_t step, size_t stage);
+};
+
+}  // namespace titaev_m_sortirovka_betchera
