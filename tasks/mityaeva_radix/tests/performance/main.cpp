d070ed3dc1f390527ac91954d39611f779809df3
daasmit07 <daas.mit07@gmail.com>
2026-04-08 14:33:34 +0300

    Update main.cpp

--- tasks/mityaeva_radix/tests/performance/main.cpp
+++ tasks/mityaeva_radix/tests/performance/main.cpp
@@ -5,9 +5,12 @@
 
 #include "mityaeva_radix/common/include/common.hpp"
 #include "mityaeva_radix/common/include/test_generator.hpp"
+#include "mityaeva_radix/omp/include/ops_omp.hpp"
 #include "mityaeva_radix/seq/include/ops_seq.hpp"
 #include "mityaeva_radix/stl/include/ops_stl.hpp"
+#include "mityaeva_radix/tbb/include/ops_omp.hpp"
 #include "util/include/perf_test_util.hpp"
+
 namespace mityaeva_radix {
 
 class MityaevaRadixPerf : public ppc::util::BaseRunPerfTests<InType, OutType> {
@@ -34,7 +37,7 @@ TEST_P(MityaevaRadixPerf, RadixSortPerf) {
 namespace {
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, MityaevaRadixSeq, MityaevaRadixStl>(PPC_SETTINGS_mityaeva_radix);
+    ppc::util::MakeAllPerfTasks<InType, MityaevaRadixSeq, MityaevaRadixOmp, MityaevaRadixTbb, MityaevaRadixStl>(PPC_SETTINGS_mityaeva_radix);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
