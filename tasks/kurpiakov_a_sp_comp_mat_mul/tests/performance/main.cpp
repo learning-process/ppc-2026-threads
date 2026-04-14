4c46cab7aa248c18a8b814f202a67f3ccf4d18cd
AlKurpiakov <s23380350@unn.ru>
2026-04-14 13:16:10 +0300

    Changes to be committed: 	modified:   tasks/kurpiakov_a_sp_comp_mat_mul/common/include/common.hpp 	modified:   tasks/kurpiakov_a_sp_comp_mat_mul/tests/performance/main.cpp

--- tasks/kurpiakov_a_sp_comp_mat_mul/tests/performance/main.cpp
+++ tasks/kurpiakov_a_sp_comp_mat_mul/tests/performance/main.cpp
@@ -69,8 +69,8 @@ TEST_P(KurpiakovRunPerfTests, SparseMatMulPerf) {
 namespace {
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, KurpiskovACRSMatMulSEQ, KurpiakovACRSMatMulOMP, KurpiakovACRSMatMulTBB, KurpiakovACRSMatMulOMP>(
-        PPC_SETTINGS_kurpiakov_a_sp_comp_mat_mul);
+    ppc::util::MakeAllPerfTasks<InType, KurpiskovACRSMatMulSEQ, KurpiakovACRSMatMulOMP, KurpiakovACRSMatMulTBB,
+                                KurpiakovACRSMatMulOMP>(PPC_SETTINGS_kurpiakov_a_sp_comp_mat_mul);
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 const auto kPerfTestName = KurpiakovRunPerfTests::CustomPerfTestName;
 
