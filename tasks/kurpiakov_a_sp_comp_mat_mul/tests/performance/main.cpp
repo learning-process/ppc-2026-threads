ac77f0c2b51fa73563dd81cbb862416dee311f29
Aleksei Kurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2026-04-14 08:49:03 +0300

    Update main.cpp

--- tasks/kurpiakov_a_sp_comp_mat_mul/tests/performance/main.cpp
+++ tasks/kurpiakov_a_sp_comp_mat_mul/tests/performance/main.cpp
@@ -4,7 +4,9 @@
 #include <vector>
 
 #include "kurpiakov_a_sp_comp_mat_mul/common/include/common.hpp"
+#include "kurpiakov_a_sp_comp_mat_mul/omp/include/ops_omp.hpp"
 #include "kurpiakov_a_sp_comp_mat_mul/seq/include/ops_seq.hpp"
+#include "kurpiakov_a_sp_comp_mat_mul/tbb/include/ops_tbb.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace kurpiakov_a_sp_comp_mat_mul {
