053e262a5422636a29afdd97b6a3438d379f9327
unknown <nik.ovsyannikov2090@gmail.com>
2026-04-19 23:36:39 +0300

    fixed ct

--- tasks/ovsyannikov_n_simpson_method/all/src/ops_all.cpp
+++ tasks/ovsyannikov_n_simpson_method/all/src/ops_all.cpp
@@ -7,6 +7,7 @@
 #include <atomic>
 #include <cmath>
 #include <functional>
+#include <numeric>
 #include <thread>
 #include <vector>
 
@@ -41,8 +42,10 @@ bool OvsyannikovNSimpsonMethodALL::PreProcessingImpl() {
 }
 
 bool OvsyannikovNSimpsonMethodALL::RunImpl() {
-  int rank = -1;
+  int rank = 0;
+  int size = 1;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   const int nx_l = params_.nx;
   const int ny_l = params_.ny;
@@ -51,47 +54,43 @@ bool OvsyannikovNSimpsonMethodALL::RunImpl() {
   const double hx = (params_.bx - params_.ax) / nx_l;
   const double hy = (params_.by - params_.ay) / ny_l;
 
-  double total_sum = 0.0;
+  int total_rows = nx_l + 1;
+  int rows_per_rank = total_rows / size;
+  int remainder = total_rows % size;
 
-#pragma omp parallel default(none) shared(nx_l, ny_l, ax_l, ay_l, hx, hy) reduction(+ : total_sum)
-  {
-#pragma omp for
-    for (int i = 0; i <= nx_l; ++i) {
-      const double x = ax_l + (static_cast<double>(i) * hx);
-      const double coeff_x = GetCoeff(i, nx_l);
+  int my_start = rank * rows_per_rank + std::min(rank, remainder);
+  int my_end = my_start + rows_per_rank + (rank < remainder ? 1 : 0);
 
-      double row_sum = tbb::parallel_reduce(tbb::blocked_range<int>(0, ny_l + 1), 0.0,
-                                            [&](const tbb::blocked_range<int> &r, double local_sum) {
-        for (int j = r.begin(); j < r.end(); ++j) {
-          const double y = ay_l + (static_cast<double>(j) * hy);
-          const double coeff_y = GetCoeff(j, ny_l);
-          local_sum += coeff_y * Function(x, y);
-        }
-        return local_sum;
-      }, std::plus<>());
-      total_sum += coeff_x * row_sum;
-    }
+  double local_sum = 0.0;
+
+#pragma omp parallel for default(none) shared(my_start, my_end, nx_l, ny_l, ax_l, ay_l, hx, hy) reduction(+ : local_sum)
+  for (int i = my_start; i < my_end; ++i) {
+    const double x = ax_l + (static_cast<double>(i) * hx);
+    const double coeff_x = GetCoeff(i, nx_l);
+
+    double row_sum = tbb::parallel_reduce(tbb::blocked_range<int>(0, ny_l + 1), 0.0,
+                                          [&](const tbb::blocked_range<int> &r, double sum) {
+      for (int j = r.begin(); j < r.end(); ++j) {
+        const double y = ay_l + (static_cast<double>(j) * hy);
+        const double coeff_y = GetCoeff(j, ny_l);
+        sum += coeff_y * Function(x, y);
+      }
+      return sum;
+    }, std::plus<>());
+
+    local_sum += coeff_x * row_sum;
   }
 
-  double global_sum = 0.0;
-  MPI_Reduce(&total_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  double total_sum = 0.0;
+  MPI_Reduce(&local_sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
   if (rank == 0) {
-    int world_size = 0;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    res_ = (hx * hy / 9.0) * (global_sum / world_size);
+    res_ = (hx * hy / 9.0) * total_sum;
   }
 
-  const int num_threads = ppc::util::GetNumThreads();
   std::atomic<int> counter(0);
-  std::vector<std::thread> threads;
-  threads.reserve(num_threads);
-  for (int i = 0; i < num_threads; i++) {
-    threads.emplace_back([&]() { counter++; });
-  }
-  for (auto &t : threads) {
-    t.join();
-  }
+  std::thread t([&]() { counter++; });
+  t.join();
 
   MPI_Barrier(MPI_COMM_WORLD);
   return true;
