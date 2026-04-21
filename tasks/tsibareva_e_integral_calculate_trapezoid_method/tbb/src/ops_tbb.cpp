445b79a30d50b7d3e46a31b0d4c064e78c452fdb
etsibareva <etsibareva.university@gmail.com>
2026-04-22 00:26:24 +0300

    result

--- tasks/tsibareva_e_integral_calculate_trapezoid_method/tbb/src/ops_tbb.cpp
+++ tasks/tsibareva_e_integral_calculate_trapezoid_method/tbb/src/ops_tbb.cpp
@@ -29,21 +29,21 @@ bool TsibarevaEIntegralCalculateTrapezoidMethodTBB::PreProcessingImpl() {
 }
 
 bool TsibarevaEIntegralCalculateTrapezoidMethodTBB::RunImpl() {
-  const Integral &inputI = GetInput();
-  int dim = inputI.dim;
+  const Integral &input_i = GetInput();
+  int dim = input_i.dim;
 
   std::vector<double> h(dim);
   std::vector<int> sizes(dim);
   int total_nodes = 1;
   for (int i = 0; i < dim; ++i) {
-    h[i] = (inputI.hi[i] - inputI.lo[i]) / inputI.steps[i];
-    sizes[i] = inputI.steps[i] + 1;
+    h[i] = (input_i.hi[i] - input_i.lo[i]) / input_i.steps[i];
+    sizes[i] = input_i.steps[i] + 1;
     total_nodes *= sizes[i];
   }
 
   double res_sum = tbb::parallel_reduce(tbb::blocked_range<int>(0, total_nodes), 0.0,
                                         [&](const tbb::blocked_range<int> &r, double local_sum) {
-    return local_sum + ComputeSumNode(r, 0.0, inputI, h, sizes);
+    return local_sum + ComputeSumNode(r, 0.0, input_i, h, sizes);
   }, std::plus<>());
 
   double res_h = 1.0;
@@ -59,11 +59,11 @@ bool TsibarevaEIntegralCalculateTrapezoidMethodTBB::PostProcessingImpl() {
 }
 
 double TsibarevaEIntegralCalculateTrapezoidMethodTBB::ComputeSumNode(const tbb::blocked_range<int> &range, double init,
-                                                                     const Integral &inputI,
+                                                                     const Integral &input_i,
                                                                      const std::vector<double> &h,
                                                                      const std::vector<int> &sizes) {
   double sum = init;
-  int dim = inputI.dim;
+  int dim = input_i.dim;
 
   for (int node = range.begin(); node != range.end(); ++node) {
     int rem = node;
@@ -74,14 +74,14 @@ double TsibarevaEIntegralCalculateTrapezoidMethodTBB::ComputeSumNode(const tbb::
       int idx = rem % sizes[i];
       rem /= sizes[i];
 
-      if (idx == 0 || idx == inputI.steps[i]) {
+      if (idx == 0 || idx == input_i.steps[i]) {
         node_w *= 0.5;
       }
 
-      point[i] = inputI.lo[i] + (idx * h[i]);
+      point[i] = input_i.lo[i] + (idx * h[i]);
     }
 
-    sum += node_w * inputI.f(point);
+    sum += node_w * input_i.f(point);
   }
 
   return sum;
