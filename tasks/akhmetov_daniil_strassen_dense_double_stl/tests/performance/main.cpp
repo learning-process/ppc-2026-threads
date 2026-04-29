1e1f27527ed4a7c4c577d33221c2b6b1a9f81390
TheNarhawl <danah29122003@gmail.com>
2026-04-29 19:02:07 +0300

    initial

---
+++ tasks/akhmetov_daniil_strassen_dense_double_stl/tests/performance/main.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+
+#include <cstddef>
+#include <random>
+
+#include "akhmetov_daniil_strassen_dense_double_stl/common/include/common.hpp"
+#include "akhmetov_daniil_strassen_dense_double_stl/stl/include/ops_stl.hpp"
+#include "util/include/perf_test_util.hpp"
+
+namespace akhmetov_daniil_strassen_dense_double_stl {
+
+class AkhmetovDaniilRunPerfTestsSTL : public ppc::util::BaseRunPerfTests<InType, OutType> {
+ public:
+  void SetUp() override {
+    const TestType n = 1024;
+
+    input_data_.resize(1 + (2 * n * n));
+    input_data_[0] = static_cast<double>(n);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    for (size_t i = 1; i < input_data_.size(); ++i) {
+      input_data_[i] = dist(gen);
+    }
+  }
+
+  bool CheckTestOutputData(OutType &output_data) override {
+    if (output_data.empty()) {
+      return false;
+    }
+
+    const size_t n = format::GetN(input_data_);
+    return output_data.size() == n * n;
+  }
+
+  InType GetTestInputData() override {
+    return input_data_;
+  }
+
+ private:
+  InType input_data_;
+};
+
+namespace {
+
+TEST_P(AkhmetovDaniilRunPerfTestsSTL, StrassenTestPerf) {
+  ExecuteTest(GetParam());
+}
+
+const auto kAllPerfTasks = ppc::util::MakeAllPerfTasks<InType, AkhmetovDStrassenDenseDoubleSTL>(
+    PPC_SETTINGS_akhmetov_daniil_strassen_dense_double_stl);
+
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+const auto kPerfTestName = AkhmetovDaniilRunPerfTestsSTL::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunStrassenPerfTestsSTL, AkhmetovDaniilRunPerfTestsSTL, kGtestValues, kPerfTestName);
+
+}  // namespace
+}  // namespace akhmetov_daniil_strassen_dense_double_stl
