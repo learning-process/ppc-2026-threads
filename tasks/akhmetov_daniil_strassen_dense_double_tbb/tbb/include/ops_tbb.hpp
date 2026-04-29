c22597a10cee36c5e783d932a1df12f6a7808d24
TheNarhawl <danah29122003@gmail.com>
2026-04-29 17:59:44 +0300

    initial

---
+++ tasks/akhmetov_daniil_strassen_dense_double_tbb/tbb/include/ops_tbb.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "akhmetov_daniil_strassen_dense_double_tbb/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace akhmetov_daniil_strassen_dense_double_tbb {
+
+class AkhmetovDStrassenDenseDoubleTBB : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kTBB;
+  }
+  explicit AkhmetovDStrassenDenseDoubleTBB(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace akhmetov_daniil_strassen_dense_double_tbb
