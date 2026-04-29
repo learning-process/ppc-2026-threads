9575da85b60b75dc8243f583c1c32b630cc68c18
1Dura <BeSTaR_X@mail.ru>
2026-04-29 16:26:34 +0300

    retry

--- tasks/ovchinnikov_m_shell_sort_batcher_merge/tbb/src/ops_tbb.cpp
+++ tasks/ovchinnikov_m_shell_sort_batcher_merge/tbb/src/ops_tbb.cpp
@@ -11,7 +11,7 @@
 
 #include "ovchinnikov_m_shell_sort_batcher_merge/common/include/common.hpp"
 #include "util/include/util.hpp"
-
+// nothing
 namespace ovchinnikov_m_shell_sort_batcher_merge {
 
 namespace {
