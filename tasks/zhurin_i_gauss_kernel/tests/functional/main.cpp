31647928d99f8279419080cc1dd4726e98bf5a87
VesTnik1320 <zhurin.ivan33@outlook.com>
2026-05-04 15:09:58 +0300

    Remove unused task definitions from main.cpp
    Removed sequential, OpenMP, and TBB task definitions from the main test file, retaining only STL tasks.

--- tasks/zhurin_i_gauss_kernel/tests/functional/main.cpp
+++ tasks/zhurin_i_gauss_kernel/tests/functional/main.cpp
@@ -11,9 +11,6 @@
 #include "task/include/task.hpp"
 #include "util/include/func_test_util.hpp"
 #include "zhurin_i_gauss_kernel/common/include/common.hpp"
-#include "zhurin_i_gauss_kernel/omp/include/ops_omp.hpp"
-#include "zhurin_i_gauss_kernel/seq/include/ops_seq.hpp"
-#include "zhurin_i_gauss_kernel/stl/include/ops_stl.hpp"
 #include "zhurin_i_gauss_kernel/tbb/include/ops_tbb.hpp"
 
 namespace zhurin_i_gauss_kernel {
@@ -81,15 +78,9 @@ const std::array<TestCase, 6> kTestCases = {
      {6, MakeInput(4, 4, 4, std::vector<std::vector<int>>(4, std::vector<int>(4, 0))),
       OutType(4, std::vector<int>(4, 0))}}};
 
-const auto kSeqTasks =
-    ppc::util::AddFuncTask<ZhurinIGaussKernelSEQ, InType>(kTestCases, PPC_SETTINGS_zhurin_i_gauss_kernel);
-const auto kOmpTasks =
-    ppc::util::AddFuncTask<ZhurinIGaussKernelOMP, InType>(kTestCases, PPC_SETTINGS_zhurin_i_gauss_kernel);
-const auto kTbbTasks =
-    ppc::util::AddFuncTask<ZhurinIGaussKernelTBB, InType>(kTestCases, PPC_SETTINGS_zhurin_i_gauss_kernel);
 const auto kStlTasks =
     ppc::util::AddFuncTask<ZhurinIGaussKernelSTL, InType>(kTestCases, PPC_SETTINGS_zhurin_i_gauss_kernel);
-const auto kAllTasksList = std::tuple_cat(kSeqTasks, kOmpTasks, kTbbTasks, kStlTasks);
+const auto kAllTasksList = std::tuple_cat(kStlTasks);
 
 inline const auto kGtestValues = ppc::util::ExpandToValues(kAllTasksList);
 
