b440b5549cd5c980e6a9244485f81401aa677913
NikaKrasnopevtseva <Krasnopevtseva_veronika@mail.ru>
2026-05-08 12:09:21 +0000

    Edit tests

--- tasks/krasnopevtseva_v_hoare_batcher_sort/tests/performance/main.cpp
+++ tasks/krasnopevtseva_v_hoare_batcher_sort/tests/performance/main.cpp
@@ -8,7 +8,7 @@
 #include "krasnopevtseva_v_hoare_batcher_sort/common/include/common.hpp"
 #include "krasnopevtseva_v_hoare_batcher_sort/omp/include/ops_omp.hpp"
 #include "krasnopevtseva_v_hoare_batcher_sort/seq/include/ops_seq.hpp"
-#include "krasnopevtseva_v_hoare_batcher_sort/tbb/include/ops_tbb.hpp"
+#include "krasnopevtseva_v_hoare_batcher_sort/stl/include/ops_stl.hpp"
 #include "util/include/perf_test_util.hpp"
 
 namespace krasnopevtseva_v_hoare_batcher_sort {
@@ -52,7 +52,7 @@ namespace {
 
 const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, KrasnopevtsevaVHoareBatcherSortSEQ, KrasnopevtsevaVHoareBatcherSortOMP,
-                                KrasnopevtsevaVHoareBatcherSortTBB>(PPC_SETTINGS_krasnopevtseva_v_hoare_batcher_sort);
+                                KrasnopevtsevaVHoareBatcherSortSTL>(PPC_SETTINGS_krasnopevtseva_v_hoare_batcher_sort);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
