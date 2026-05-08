bb93ddc4f5619541cbff2f5ec9cce94c2f93c39d
NikaKrasnopevtseva <Krasnopevtseva_veronika@mail.ru>
2026-05-08 12:08:52 +0000

    Delete report file

--- tasks/krasnopevtseva_v_hoare_batcher_sort/stl/src/ops_stl.cpp
+++ tasks/krasnopevtseva_v_hoare_batcher_sort/stl/src/ops_stl.cpp
@@ -1,10 +1,9 @@
 #include "krasnopevtseva_v_hoare_batcher_sort/stl/include/ops_stl.hpp"
 
-
 #include <algorithm>
 #include <cstddef>
-#include <stack>
 #include <future>
+#include <stack>
 #include <thread>
 #include <utility>
 #include <vector>
@@ -32,19 +31,27 @@ bool KrasnopevtsevaVHoareBatcherSortSTL::PreProcessingImpl() {
 bool KrasnopevtsevaVHoareBatcherSortSTL::RunImpl() {
   const auto &input = GetInput();
   std::size_t size = input.size();
-  
+
   if (size <= 1) {
     GetOutput() = input;
     return true;
   }
-  
+
   std::vector<int> res = input;
 
   int n = static_cast<int>(size);
   int numthreads = static_cast<int>(std::thread::hardware_concurrency());
-  if (numthreads == 0) numthreads = 1;
+  if (numthreads == 0) {
+    numthreads = 1;
+  }
   numthreads = std::min(n, numthreads);
-  
+
+  if (n < 1000) {
+    QuickSort(res, 0, n - 1);
+    GetOutput() = std::move(res);
+    return true;
+  }
+
   int thread_input_size = n / numthreads;
   int thread_input_remainder_size = n % numthreads;
 
@@ -57,21 +64,54 @@ bool KrasnopevtsevaVHoareBatcherSortSTL::RunImpl() {
   }
   sizes[sizes.size() - 1] += thread_input_remainder_size;
 
-  // Параллельная сортировка блоков через std::async
-  std::vector<std::future<void>> futures;
+  std::vector<std::thread> threads;
+  threads.reserve(numthreads);
+
   for (int i = 0; i < numthreads; ++i) {
-    futures.push_back(std::async(std::launch::async, [&res, &pointers, &sizes, i]() {
+    threads.emplace_back([&res, &pointers, &sizes, i]() {
       int left = static_cast<int>(pointers[i] - res.data());
       int right = left + sizes[i] - 1;
       QuickSort(res, left, right);
-    }));
+    });
   }
-  for (auto &f : futures) {
-    f.wait();
+
+  for (auto &t : threads) {
+    t.join();
   }
-  
-  BatcherMerge(thread_input_size, pointers, sizes, 32);
-  
+
+  std::vector<int> temp(n);
+  int step = 1;
+
+  while (step < numthreads) {
+    for (int i = 0; i < numthreads; i += 2 * step) {
+      int left_block = i;
+      int mid_block = std::min(i + step, numthreads);
+      int right_block = std::min(i + 2 * step, numthreads);
+
+      int left_start = (left_block == 0) ? 0 : static_cast<int>(pointers[left_block] - res.data());
+      int left_end = static_cast<int>(pointers[mid_block] - res.data()) - 1;
+      int right_start = left_end + 1;
+      int right_end = (right_block == numthreads) ? n - 1 : static_cast<int>(pointers[right_block] - res.data()) - 1;
+
+      int i1 = left_start, i2 = right_start, pos = left_start;
+      while (i1 <= left_end && i2 <= right_end) {
+        if (res[i1] <= res[i2]) {
+          temp[pos++] = res[i1++];
+        } else {
+          temp[pos++] = res[i2++];
+        }
+      }
+      while (i1 <= left_end) {
+        temp[pos++] = res[i1++];
+      }
+      while (i2 <= right_end) {
+        temp[pos++] = res[i2++];
+      }
+    }
+    std::copy(temp.begin(), temp.end(), res.begin());
+    step *= 2;
+  }
+
   GetOutput() = std::move(res);
   return true;
 }
@@ -114,7 +154,9 @@ void KrasnopevtsevaVHoareBatcherSortSTL::QuickSort(std::vector<int> &arr, int fi
     auto [l, r] = stack.top();
     stack.pop();
 
-    if (l >= r) continue;
+    if (l >= r) {
+      continue;
+    }
 
     if (r - l < 16) {
       InsertionSort(arr, l, r);
@@ -133,14 +175,14 @@ void KrasnopevtsevaVHoareBatcherSortSTL::QuickSort(std::vector<int> &arr, int fi
   }
 }
 
-void KrasnopevtsevaVHoareBatcherSortSTL::BatcherMergeBlocksStep(int *left_pointer, int &left_size, 
-                                                                  int *right_pointer, int &right_size) {
+void KrasnopevtsevaVHoareBatcherSortSTL::BatcherMergeBlocksStep(int *left_pointer, int &left_size, int *right_pointer,
+                                                                int &right_size) {
   std::inplace_merge(left_pointer, right_pointer, right_pointer + right_size);
   left_size += right_size;
 }
 
 void KrasnopevtsevaVHoareBatcherSortSTL::BatcherMerge(int thread_input_size, std::vector<int *> &pointers,
-                                                       std::vector<int> &sizes, int par_if_greater) {
+                                                      std::vector<int> &sizes, int par_if_greater) {
   int pack = static_cast<int>(pointers.size());
   for (int step = 1; pack > 1; step *= 2, pack /= 2) {
     if ((thread_input_size / step) > par_if_greater) {
@@ -162,10 +204,10 @@ void KrasnopevtsevaVHoareBatcherSortSTL::BatcherMerge(int thread_input_size, std
         BatcherMergeBlocksStep(pointers[idx1], sizes[idx1], pointers[idx2], sizes[idx2]);
       }
     }
-    
+
     if ((pack / 2) - 1 == 0) {
-      BatcherMergeBlocksStep(pointers[0], sizes[sizes.size() - 1], 
-                             pointers[pointers.size() - 1], sizes[sizes.size() - 1]);
+      BatcherMergeBlocksStep(pointers[0], sizes[sizes.size() - 1], pointers[pointers.size() - 1],
+                             sizes[sizes.size() - 1]);
     } else if ((pack / 2) % 2 != 0) {
       auto idx1 = static_cast<std::size_t>(2 * step) * static_cast<std::size_t>((pack / 2) - 2);
       auto idx2 = static_cast<std::size_t>(2 * step) * static_cast<std::size_t>((pack / 2) - 1);
