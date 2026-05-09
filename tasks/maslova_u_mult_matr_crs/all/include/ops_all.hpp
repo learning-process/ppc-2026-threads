910cefc22adfd1d4d2c9a74dc3bf3da2509ed5e9
ulianamaslova25 <uliana-maslova@mail.ru>
2026-05-09 18:23:55 +0300

    add all

---
+++ tasks/maslova_u_mult_matr_crs/all/include/ops_all.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+
+#include "maslova_u_mult_matr_crs/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace maslova_u_mult_matr_crs {
+
+class MaslovaUMultMatrALL : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kALL;
+  }
+  explicit MaslovaUMultMatrALL(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+  void BroadcastCRSMatrix(CRSMatrix &matrix, int root, int rows, int cols);
+
+  void ComputeLocalPart(const CRSMatrix &a, const CRSMatrix &b, int start_row, int local_rows,
+                        std::vector<int> &local_nnz, std::vector<double> &flat_values, std::vector<int> &flat_cols);
+
+  void GatherResults(int rank, int size, int a_rows, int b_cols, int local_rows, const std::vector<int> &local_nnz,
+                     const std::vector<double> &flat_values, const std::vector<int> &flat_cols);
+};
+
+}  // namespace maslova_u_mult_matr_crs
