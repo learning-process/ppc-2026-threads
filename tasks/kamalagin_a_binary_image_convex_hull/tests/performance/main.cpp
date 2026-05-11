d510a2e0ccac625c80d223ab2372d34b92010c47
Lynxll <mrspider0530@gmail.com>
2026-05-11 16:33:56 +0300

    kamalagin_a_binary_image_convex_hull: add TBB implementation

--- tasks/kamalagin_a_binary_image_convex_hull/tests/performance/main.cpp
+++ tasks/kamalagin_a_binary_image_convex_hull/tests/performance/main.cpp
@@ -7,6 +7,7 @@
 #include "kamalagin_a_binary_image_convex_hull/common/include/common.hpp"
 #include "kamalagin_a_binary_image_convex_hull/omp/include/ops_omp.hpp"
 #include "kamalagin_a_binary_image_convex_hull/seq/include/ops_seq.hpp"
+#include "kamalagin_a_binary_image_convex_hull/tbb/include/ops_tbb.hpp"
 #include "performance/include/performance.hpp"
 #include "util/include/perf_test_util.hpp"
 
@@ -77,8 +78,8 @@ TEST_P(KamalaginRunPerfTests, RunPerfModes) {
 namespace {
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, KamalaginABinaryImageConvexHullSEQ, KamalaginABinaryImageConvexHullOMP>(
-        PPC_SETTINGS_kamalagin_a_binary_image_convex_hull);
+    ppc::util::MakeAllPerfTasks<InType, KamalaginABinaryImageConvexHullSEQ, KamalaginABinaryImageConvexHullOMP,
+                                KamalaginABinaryImageConvexHullTBB>(PPC_SETTINGS_kamalagin_a_binary_image_convex_hull);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 const auto kPerfTestName = KamalaginRunPerfTests::CustomPerfTestName;
