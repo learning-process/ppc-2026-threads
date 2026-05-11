923de7b682dcce2e8056a8f8a29946434a2a029b
Ch0k0ladka <ch0ko0ladka@gmail.com>
2026-05-11 15:53:34 +0300

    feat: stl

--- tasks/iskhakov_d_vertical_gauss_filter/tests/performance/main.cpp
+++ tasks/iskhakov_d_vertical_gauss_filter/tests/performance/main.cpp
@@ -8,6 +8,7 @@
 #include "iskhakov_d_vertical_gauss_filter/common/include/common.hpp"
 #include "iskhakov_d_vertical_gauss_filter/omp/include/ops_omp.hpp"
 #include "iskhakov_d_vertical_gauss_filter/seq/include/ops_seq.hpp"
+#include "iskhakov_d_vertical_gauss_filter/stl/include/ops_stl.hpp"
 #include "iskhakov_d_vertical_gauss_filter/tbb/include/ops_tbb.hpp"
 #include "util/include/perf_test_util.hpp"
 
@@ -50,7 +51,8 @@ namespace {
 
 const auto kAllPerfTasks =
     ppc::util::MakeAllPerfTasks<InType, IskhakovDVerticalGaussFilterSEQ, IskhakovDVerticalGaussFilterOMP,
-                                IskhakovDVerticalGaussFilterTBB>(PPC_SETTINGS_iskhakov_d_vertical_gauss_filter);
+                                IskhakovDVerticalGaussFilterTBB, IskhakovDVerticalGaussFilterSTL>(
+        PPC_SETTINGS_iskhakov_d_vertical_gauss_filter);
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
