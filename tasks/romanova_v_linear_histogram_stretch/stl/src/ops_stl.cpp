b77a8e0a1f9160c75f9cf63fe0c841be9beb4efd
vassyusha <romanoffmiss@yandex.ru>
2026-05-13 01:17:10 +0300

    stl version is done

--- tasks/romanova_v_linear_histogram_stretch/stl/src/ops_stl.cpp
+++ tasks/romanova_v_linear_histogram_stretch/stl/src/ops_stl.cpp
@@ -1,7 +1,9 @@
 #include "romanova_v_linear_histogram_stretch/stl/include/ops_stl.hpp"
 
+#include <algorithm>
 #include <atomic>
 #include <numeric>
+#include <ranges>
 #include <thread>
 #include <vector>
 
@@ -9,6 +11,15 @@
 #include "util/include/util.hpp"
 
 namespace romanova_v_linear_histogram_stretch_threads {
+
+void RomanovaVLinHistogramStretchSTL::GetThreadRange(size_t thid, size_t total, size_t num_th, size_t &beg,
+                                                     size_t &en) {
+  size_t extra = total % num_th;
+  size_t delta = total / num_th;
+  size_t chunk = delta + (thid < extra ? 1 : 0);
+  beg = (thid < extra ? thid * chunk : extra * (chunk + 1) + (thid - extra) * chunk);  
+  en = beg + chunk; 
+}
 
 RomanovaVLinHistogramStretchSTL::RomanovaVLinHistogramStretchSTL(const InType &in) {
   SetTypeOfTask(GetStaticTypeOfTask());
@@ -26,6 +37,69 @@ bool RomanovaVLinHistogramStretchSTL::PreProcessingImpl() {
 }
 
 bool RomanovaVLinHistogramStretchSTL::RunImpl() {
+  const InType &in = GetInput();
+  OutType &out = GetOutput();
+  size_t size = in.size();
+
+  const int num_th = ppc::util::GetNumThreads();
+  std::vector<std::thread> threads;
+  threads.reserve(num_th);
+
+  std::vector<uint8_t> local_min(num_th, 255);
+  std::vector<uint8_t> local_max(num_th, 0);
+
+  for (int thid = 0; thid < num_th; thid++) {
+    threads.emplace_back([&, thid]() {
+      size_t begin = 0;
+      size_t end = 0;
+
+      GetThreadRange(thid, size, num_th, begin, end);
+
+      for (size_t i = begin; i < end; i++) {
+        local_min[thid] = std::min(local_min[thid], in[i]);
+        local_max[thid] = std::max(local_max[thid], in[i]);
+      }
+    });
+  }
+
+  for (auto &th : threads) {
+    th.join();
+  }
+  threads.clear();
+
+  uint8_t min_v = local_min[0];
+  uint8_t max_v = local_max[0];
+  for (int thid = 1; thid < num_th; thid++) {
+    min_v = std::min(min_v, local_min[thid]);
+    max_v = std::max(max_v, local_max[thid]);
+  }
+
+  if (min_v == max_v) {
+    std::ranges::copy(in, out.begin());
+    return true;
+  }
+
+  const uint8_t diff = max_v - min_v;
+  const double ratio = 255.0 / diff;
+
+  for (int thid = 0; thid < num_th; thid++) {
+    threads.emplace_back([&, thid]() {
+      size_t begin = 0;
+      size_t end = 0;
+
+      GetThreadRange(thid, size, num_th, begin, end);
+
+      for (size_t i = begin; i < end; i++) {
+        out[i] = (std::clamp(static_cast<int>((in[i] - min_v) * ratio), 0, 255));
+      }
+    });
+  }
+
+  for (auto &th : threads) {
+    th.join();
+  }
+  threads.clear();
+
   return true;
 }
 
