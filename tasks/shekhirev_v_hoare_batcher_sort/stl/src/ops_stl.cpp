38a78eb461dcf3e4b865e46eb3048bbe05b30dbf
Lepsps <gamarfastrast@gmail.com>
2026-05-13 14:29:11 +0000

    clang-tidy fix

--- tasks/shekhirev_v_hoare_batcher_sort/stl/src/ops_stl.cpp
+++ tasks/shekhirev_v_hoare_batcher_sort/stl/src/ops_stl.cpp
@@ -6,9 +6,31 @@
 #include <utility>
 #include <vector>
 
+#include "shekhirev_v_hoare_batcher_sort/common/include/common.hpp"
+
 namespace shekhirev_v_hoare_batcher_sort {
 
 namespace {
+
+void SplitPartition(std::vector<int> &arr, int &l, int &r, int &i, int &j) {
+  int pivot = arr[l + ((r - l) / 2)];
+  i = l;
+  j = r;
+
+  while (i <= j) {
+    while (arr[i] < pivot) {
+      i++;
+    }
+    while (arr[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      std::swap(arr[i], arr[j]);
+      i++;
+      j--;
+    }
+  }
+}
 
 void OptimizedHoareSort(std::vector<int> &arr, int left, int right) {
   if (left >= right) {
@@ -24,23 +46,9 @@ void OptimizedHoareSort(std::vector<int> &arr, int left, int right) {
     stack.pop_back();
 
     while (l < r) {
-      int pivot = arr[l + (r - l) / 2];
-      int i = l;
-      int j = r;
-
-      while (i <= j) {
-        while (arr[i] < pivot) {
-          i++;
-        }
-        while (arr[j] > pivot) {
-          j--;
-        }
-        if (i <= j) {
-          std::swap(arr[i], arr[j]);
-          i++;
-          j--;
-        }
-      }
+      int i = 0;
+      int j = 0;
+      SplitPartition(arr, l, r, i, j);
 
       if (j - l < r - i) {
         if (i < r) {
@@ -58,7 +66,7 @@ void OptimizedHoareSort(std::vector<int> &arr, int left, int right) {
 }
 
 void MergeBlocks(std::vector<int> &arr, int start1, int start2, int chunk_size) {
-  std::vector<int> buffer(chunk_size * 2);
+  std::vector<int> buffer(static_cast<size_t>(chunk_size) * 2);
   int i = start1;
   int j = start2;
   int k = 0;
@@ -81,9 +89,33 @@ void MergeBlocks(std::vector<int> &arr, int start1, int start2, int chunk_size) 
     buffer[k++] = arr[j++];
   }
 
-  for (int x = 0; x < chunk_size; ++x) {
-    arr[start1 + x] = buffer[x];
-    arr[start2 + x] = buffer[chunk_size + x];
+  for (int idx = 0; idx < chunk_size; ++idx) {
+    arr[start1 + idx] = buffer[idx];
+    arr[start2 + idx] = buffer[chunk_size + idx];
+  }
+}
+
+void BatcherMergePhase(std::vector<int> &data, int num_threads, int chunk_size) {
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+
+  for (int step_p = 1; step_p < num_threads; step_p *= 2) {
+    for (int step_k = step_p; step_k > 0; step_k /= 2) {
+      for (int idx = 0; idx < num_threads - step_k; ++idx) {
+        if ((idx / (step_p * 2)) == ((idx + step_k) / (step_p * 2))) {
+          int start_a = idx * chunk_size;
+          int start_b = (idx + step_k) * chunk_size;
+
+          threads.emplace_back(
+              [&data, start_a, start_b, chunk_size]() { MergeBlocks(data, start_a, start_b, chunk_size); });
+        }
+      }
+
+      for (auto &t : threads) {
+        t.join();
+      }
+      threads.clear();
+    }
   }
 }
 
@@ -139,7 +171,7 @@ bool ShekhirevHoareBatcherSortSTL::RunImpl() {
 
   for (int i = 0; i < num_threads; ++i) {
     threads.emplace_back(
-        [&data, i, chunk_size]() { OptimizedHoareSort(data, i * chunk_size, (i + 1) * chunk_size - 1); });
+        [&data, i, chunk_size]() { OptimizedHoareSort(data, i * chunk_size, ((i + 1) * chunk_size) - 1); });
   }
 
   for (auto &t : threads) {
@@ -147,24 +179,7 @@ bool ShekhirevHoareBatcherSortSTL::RunImpl() {
   }
   threads.clear();
 
-  for (int p = 1; p < num_threads; p *= 2) {
-    for (int k = p; k > 0; k /= 2) {
-      for (int i = 0; i < num_threads - k; ++i) {
-        if ((i / (p * 2)) == ((i + k) / (p * 2))) {
-          int start_a = i * chunk_size;
-          int start_b = (i + k) * chunk_size;
-
-          threads.emplace_back(
-              [&data, start_a, start_b, chunk_size]() { MergeBlocks(data, start_a, start_b, chunk_size); });
-        }
-      }
-
-      for (auto &t : threads) {
-        t.join();
-      }
-      threads.clear();
-    }
-  }
+  BatcherMergePhase(data, num_threads, chunk_size);
 
   if (padding > 0) {
     data.resize(orig_size);
