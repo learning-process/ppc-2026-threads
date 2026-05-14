f5ef1a8a3bfee4b190e8b56983ccab6557db6ae7
ArtemijAE <desnogun2006@gmail.com>
2026-05-14 23:32:05 +0300

    all

--- tasks/ilin_a_algorithm_graham/tests/performance/main.cpp
+++ tasks/ilin_a_algorithm_graham/tests/performance/main.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <vector>
 
+#include "ilin_a_algorithm_graham/all/include/ops_all.hpp"
 #include "ilin_a_algorithm_graham/common/include/common.hpp"
 #include "ilin_a_algorithm_graham/omp/include/ops_omp.hpp"
 #include "ilin_a_algorithm_graham/seq/include/ops_seq.hpp"
@@ -52,7 +53,8 @@ const auto kAllPerfTasks =
     std::tuple_cat(ppc::util::MakeAllPerfTasks<InType, IlinAGrahamSEQ>(PPC_SETTINGS_ilin_a_algorithm_graham),
                    ppc::util::MakeAllPerfTasks<InType, IlinAGrahamOMP>(PPC_SETTINGS_ilin_a_algorithm_graham),
                    ppc::util::MakeAllPerfTasks<InType, IlinAGrahamSTL>(PPC_SETTINGS_ilin_a_algorithm_graham),
-                   ppc::util::MakeAllPerfTasks<InType, IlinAGrahamTBB>(PPC_SETTINGS_ilin_a_algorithm_graham));
+                   ppc::util::MakeAllPerfTasks<InType, IlinAGrahamTBB>(PPC_SETTINGS_ilin_a_algorithm_graham),
+                   ppc::util::MakeAllPerfTasks<InType, IlinAGrahamALL>(PPC_SETTINGS_ilin_a_algorithm_graham));
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
