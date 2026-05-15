494ea070a16431d887fde20ab76cb7ac5c4783f7
AKutergin <epta337@gmail.com>
2026-05-15 16:43:44 +0300

    STL

--- tasks/kutergin_a_multidim_trapezoid/stl/src/ops_stl.cpp
+++ tasks/kutergin_a_multidim_trapezoid/stl/src/ops_stl.cpp
@@ -8,7 +8,7 @@
 #include <utility>
 #include <vector>
 
-#include "util/include/util.hpp"
+#include "kutergin_a_multidim_trapezoid/common/include/common.hpp"
 
 namespace kutergin_a_multidim_trapezoid {
 
@@ -31,8 +31,8 @@ bool KuterginAMultidimTrapezoidSTL::PreProcessingImpl() {
 bool KuterginAMultidimTrapezoidSTL::RunImpl() {
   const auto &input = GetInput();
   const auto &borders = std::get<1>(input);
-  int n = std::get<2>(input);
-  size_t dims = borders.size();
+  const int n = std::get<2>(input);
+  const size_t dims = borders.size();
 
   size_t total_nodes = 1;
   std::vector<double> h(dims);
@@ -44,33 +44,45 @@ bool KuterginAMultidimTrapezoidSTL::RunImpl() {
     cell_volume *= h[i];
   }
 
-  int num_threads = ppc::util::GetNumThreads();
-  if (total_nodes < static_cast<size_t>(num_threads)) num_threads = static_cast<int>(total_nodes);
+  unsigned int num_threads = std::thread::hardware_concurrency();
+  if (num_threads == 0) {
+    num_threads = 2;
+  }
+
+  if (total_nodes < static_cast<size_t>(num_threads)) {
+    num_threads = static_cast<unsigned int>(total_nodes);
+  }
 
   std::vector<std::thread> threads;
   std::vector<double> partial_sums(num_threads, 0.0);
 
-  size_t chunk = total_nodes / num_threads;
-  size_t remainder = total_nodes % num_threads;
+  const size_t chunk = total_nodes / num_threads;
+  const size_t remainder = total_nodes % num_threads;
 
-  for (int i = 0; i < num_threads; ++i) {
-    size_t start = (i * chunk) + std::min(static_cast<size_t>(i), remainder);
-    size_t end = start + chunk + (static_cast<size_t>(i) < remainder ? 1 : 0);
+  for (unsigned int i = 0; i < num_threads; ++i) {
+    const size_t start = (i * chunk) + std::min(static_cast<size_t>(i), remainder);
+    const size_t end = start + chunk + (static_cast<size_t>(i) < remainder ? 1 : 0);
 
     if (start < end) {
-      threads.emplace_back([this, start, end, &h, &partial_sums, i]() {
-        partial_sums[i] = CalculateChunkSum(start, end, h);
+      threads.emplace_back([this, start, end, &h, &partial_sums, i]() noexcept {
+        try {
+          partial_sums[i] = CalculateChunkSum(start, end, h);
+        } catch (...) {
+          (void)0;
+        }
       });
     }
   }
 
   for (auto &t : threads) {
-    if (t.joinable()) t.join();
+    if (t.joinable()) {
+      t.join();
+    }
   }
 
-  double total_weight_sum = std::accumulate(partial_sums.begin(), partial_sums.end(), 0.0);
+  const double total_weight_sum = std::accumulate(partial_sums.begin(), partial_sums.end(), 0.0);
   res_ = total_weight_sum * cell_volume;
-  
+
   return std::isfinite(res_);
 }
 
@@ -83,9 +95,9 @@ double KuterginAMultidimTrapezoidSTL::CalculateChunkSum(size_t start_idx, size_t
   const auto &input = GetInput();
   const auto &func = std::get<0>(input);
   const auto &borders = std::get<1>(input);
-  int n = std::get<2>(input);
-  size_t dims = borders.size();
-  
+  const int n = std::get<2>(input);
+  const size_t dims = borders.size();
+
   std::vector<double> coords(dims);
   double chunk_sum = 0.0;
 
@@ -93,11 +105,11 @@ double KuterginAMultidimTrapezoidSTL::CalculateChunkSum(size_t start_idx, size_t
     double weight = 1.0;
     size_t temp_idx = i;
 
-    for (size_t d = 0; d < dims; ++d) {
-      int coord_idx = static_cast<int>(temp_idx % (n + 1));
-      temp_idx /= (n + 1);
+    for (size_t dim_idx = 0; dim_idx < dims; ++dim_idx) {
+      const int coord_idx = static_cast<int>(temp_idx % (static_cast<size_t>(n) + 1));
+      temp_idx /= (static_cast<size_t>(n) + 1);
 
-      coords[d] = borders[d].first + coord_idx * h[d];
+      coords[dim_idx] = borders[dim_idx].first + (static_cast<double>(coord_idx) * h[dim_idx]);
 
       if (coord_idx == 0 || coord_idx == n) {
         weight *= 0.5;
