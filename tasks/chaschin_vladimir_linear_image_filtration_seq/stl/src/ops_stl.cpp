9ef62a8f13e59c4bd13971b0f64cdfde5d90c5d7
kodt2 <kodt2005@yandex.ru>
2026-05-17 13:47:59 +0300

    clang-tidy

--- tasks/chaschin_vladimir_linear_image_filtration_seq/stl/src/ops_stl.cpp
+++ tasks/chaschin_vladimir_linear_image_filtration_seq/stl/src/ops_stl.cpp
@@ -1,11 +1,10 @@
 #include "chaschin_vladimir_linear_image_filtration_seq/stl/include/ops_stl.hpp"
 
-#include <algorithm>
-#include <future>
-#include <numeric>
-#include <thread>
-#include <utility>
-#include <vector>
+#include
+#include
+#include
+#include
+#include
 
 #include "chaschin_vladimir_linear_image_filtration_seq/common/include/common.hpp"
 
@@ -28,7 +27,7 @@ bool ChaschinVLinearFiltrationSTL::PreProcessingImpl() {
   return true;
 }
 
-inline float HorizontalFilterAtSTL(const std::vector<float> &img, int n, int x, int y) {
+inline float HorizontalFilterAtSTL(const std::vector &img, int n, int x, int y) {
   const int idx = (y * n) + x;
   if (x == 0) {
     return ((2.F * img[idx]) + img[idx + 1]) / 3.F;
@@ -39,7 +38,7 @@ inline float HorizontalFilterAtSTL(const std::vector<float> &img, int n, int x, 
   return (img[idx - 1] + (2.F * img[idx]) + img[idx + 1]) / 4.F;
 }
 
-inline float VerticalFilterAtSTL(const std::vector<float> &temp, int n, int m, int x, int y) {
+inline float VerticalFilterAtSTL(const std::vector &temp, int n, int m, int x, int y) {
   const int idx = (y * n) + x;
   if (y == 0) {
     return ((2.F * temp[idx]) + temp[idx + n]) / 3.F;
@@ -48,6 +47,24 @@ inline float VerticalFilterAtSTL(const std::vector<float> &temp, int n, int m, i
     return (temp[idx - n] + (2.F * temp[idx])) / 3.F;
   }
   return (temp[idx - n] + (2.F * temp[idx]) + temp[idx + n]) / 4.F;
+}
+
+inline void ProcessHorizontalSTL(int thread_idx, int num_threads, int m, int n, const std::vector &image,
+                                 std::vector &temp) {
+  for (int yi = thread_idx; yi < m; yi += num_threads) {
+    for (int xf = 0; xf < n; ++xf) {
+      temp[(yi * n) + xf] = HorizontalFilterAtSTL(image, n, xf, yi);
+    }
+  }
+}
+
+inline void ProcessVerticalSTL(int thread_idx, int num_threads, int m, int n, const std::vector &temp,
+                               std::vector &out) {
+  for (int yi = thread_idx; yi < m; yi += num_threads) {
+    for (int xy = 0; xy < n; ++xy) {
+      out[(yi * n) + xy] = VerticalFilterAtSTL(temp, n, m, xy, yi);
+    }
+  }
 }
 
 bool ChaschinVLinearFiltrationSTL::RunImpl() {
@@ -59,48 +76,41 @@ bool ChaschinVLinearFiltrationSTL::RunImpl() {
   auto &out = GetOutput();
   out.resize(static_cast<size_t>(n) * m);
 
-  std::vector<float> temp(static_cast<size_t>(n) * m);
+  std::vector temp(static_cast<size_t>(n) * m);
 
-  unsigned int num_threads = std::thread::hardware_concurrency();
-  if (num_threads == 0) {
-    num_threads = 2;
+  unsigned int hardware_threads = std::thread::hardware_concurrency();
+  if (hardware_threads == 0) {
+    hardware_threads = 2;
   }
+  int num_threads = static_cast(hardware_threads);
 
-  // ---------- горизонтальная фильтрация ----------
   {
-    std::vector<std::future<void>> futures;
-    for (unsigned int t = 0; t < num_threads; ++t) {
-      futures.push_back(std::async(std::launch::async, [t, num_threads, m, n, &image, &temp]() {
-        for (int yi = t; yi < m; yi += num_threads) {
-          for (int xf = 0; xf < n; ++xf) {
-            temp[(yi * n) + xf] = HorizontalFilterAtSTL(image, n, xf, yi);
-          }
-        }
-      }));
-    }
-    for (auto &f : futures) {
-      f.wait();
-    }
+    std::vector<std::future> futures;
+    futures.reserve(static_cast<size_t>(num_threads));
+    for (int thread_idx = 0; thread_idx < num_threads; ++thread_idx) {
+futures.push_back(std::async(std::launch::async, [thread_idx, num_threads, m, n, &image, &temp](https://www.google.com/search?q=) {
+ProcessHorizontalSTL(thread_idx, num_threads, m, n, image, temp);
+    }));
   }
-
-  // ---------- вертикальная фильтрация ----------
-  {
-    std::vector<std::future<void>> futures;
-    for (unsigned int t = 0; t < num_threads; ++t) {
-      futures.push_back(std::async(std::launch::async, [t, num_threads, m, n, &temp, &out]() {
-        for (int yi = t; yi < m; yi += num_threads) {
-          for (int xy = 0; xy < n; ++xy) {
-            out[(yi * n) + xy] = VerticalFilterAtSTL(temp, n, m, xy, yi);
-          }
-        }
-      }));
-    }
-    for (auto &f : futures) {
-      f.wait();
-    }
+  for (auto &f : futures) {
+    f.wait();
   }
+}
 
-  return true;
+{
+  std::vector<std::future> futures;
+  futures.reserve(static_cast<size_t>(num_threads));
+  for (int thread_idx = 0; thread_idx < num_threads; ++thread_idx) {
+futures.push_back(std::async(std::launch::async, [thread_idx, num_threads, m, n, &temp, &out](https://www.google.com/search?q=) {
+ProcessVerticalSTL(thread_idx, num_threads, m, n, temp, out);
+  }));
+}
+for (auto &f : futures) {
+  f.wait();
+}
+}
+
+return true;
 }
 
 bool ChaschinVLinearFiltrationSTL::PostProcessingImpl() {
