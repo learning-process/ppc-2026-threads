a677fcd768903d18253a582f6358d641f024a6b3
Sweetbread <risdeveau@lair.moe>
2026-05-18 09:24:55 +0300

    TBB

--- tasks/zaharov_g_linear_contrast_stretch/tests/performance/main.cpp
+++ tasks/zaharov_g_linear_contrast_stretch/tests/performance/main.cpp
@@ -11,6 +11,7 @@
 #include "zaharov_g_linear_contrast_stretch/common/include/common.hpp"
 #include "zaharov_g_linear_contrast_stretch/omp/include/ops_omp.hpp"
 #include "zaharov_g_linear_contrast_stretch/seq/include/ops_seq.hpp"
+#include "zaharov_g_linear_contrast_stretch/tbb/include/ops_tbb.hpp"
 
 namespace zaharov_g_linear_contrast_stretch {
 
@@ -82,7 +83,8 @@ TEST_P(ZaharovGRunPerfTestsLinContrStr, RunPerfModes) {
 namespace {
 
 const auto kAllPerfTasks =
-    ppc::util::MakeAllPerfTasks<InType, ZaharovGLinContrStrSEQ, ZaharovGLinContrStrOMP>(GetSettingsPath());
+    ppc::util::MakeAllPerfTasks<InType, ZaharovGLinContrStrSEQ, ZaharovGLinContrStrOMP, ZaharovGLinContrStrTBB>(
+        GetSettingsPath());
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 const auto kPerfTestName = ZaharovGRunPerfTestsLinContrStr::CustomPerfTestName;
