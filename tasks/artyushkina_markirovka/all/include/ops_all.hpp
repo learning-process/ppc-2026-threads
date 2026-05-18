643b64a33f8c6bb0f4c3ace784809f75bdd308b2
Yulia66 <ajlakejdz@gmail.com>
2026-05-18 11:02:35 +0300

    all 12

--- tasks/artyushkina_markirovka/all/include/ops_all.hpp
+++ tasks/artyushkina_markirovka/all/include/ops_all.hpp
@@ -12,7 +12,7 @@ struct NeighborOffsetAll;
 class MarkingComponentsALL : public BaseTask {
  public:
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-    return ppc::task::TypeOfTask::kSEQ;  // ИЗМЕНЕНО: c kALL на kSEQ
+    return ppc::task::TypeOfTask::kMPI;  // ИЗМЕНЕНО: используем kMPI
   }
   explicit MarkingComponentsALL(const InType &in);
 
