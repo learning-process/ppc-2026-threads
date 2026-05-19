ab4d1533a64bf3f77a0fae928dd604d9e64cb133
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-05-19 15:04:29 +0300

    clang

--- tasks/luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb/tbb/include/ops_tbb.hpp
+++ tasks/luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb/tbb/include/ops_tbb.hpp
@@ -5,23 +5,23 @@
 namespace luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb {
 
 class LuchnikovEMultOfDenseMatrixFoxAlgoritmTBB : public BaseTask {
-public:
-    static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
-        return ppc::task::TypeOfTask::kTBB;
-    }
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kTBB;
+  }
 
-    explicit LuchnikovEMultOfDenseMatrixFoxAlgoritmTBB(const InType &in);
+  explicit LuchnikovEMultOfDenseMatrixFoxAlgoritmTBB(const InType &in);
 
-private:
-    bool ValidationImpl() override;
-    bool PreProcessingImpl() override;
-    bool RunImpl() override;
-    bool PostProcessingImpl() override;
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
 
-    DenseMatrix matrix_a_;
-    DenseMatrix matrix_b_;
-    DenseMatrix result_matrix_;
-    int block_size_ = 32;
+  DenseMatrix matrix_a_;
+  DenseMatrix matrix_b_;
+  DenseMatrix result_matrix_;
+  int block_size_ = 32;
 };
 
 }  // namespace luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb
--- tasks/luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb/tests/performance/main.cpp
+++ tasks/luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb/tests/performance/main.cpp
@@ -1,26 +1,25 @@
 #include <gtest/gtest.h>
+
 #include <array>
 #include <cmath>
 #include <cstddef>
 #include <tuple>
-#include "util/include/perf_test_util.hpp"
+
 #include "luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb/common/include/common.hpp"
 #include "luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb/tbb/include/ops_tbb.hpp"
+#include "util/include/perf_test_util.hpp"
 
 namespace luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb {
 
-class LuchnikovEMultOfDenseMatrixFoxAlgoritmPerfTestTBB
-    : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class LuchnikovEMultOfDenseMatrixFoxAlgoritmPerfTestTBB : public ppc::util::BaseRunPerfTests<InType, OutType> {
  protected:
   bool CheckTestOutputData(OutType &output_data) final {
     return std::isfinite(static_cast<double>(output_data));
   }
 
   InType GetTestInputData() final {
-    static constexpr std::array<InType, 9> kBenchmarkSizes = {
-        8, 12, 24, 48, 96, 192, 384, 512, 768
-    };
-    
+    static constexpr std::array<InType, 9> kBenchmarkSizes = {8, 12, 24, 48, 96, 192, 384, 512, 768};
+
     static thread_local size_t run_counter = 0;
     InType size = kBenchmarkSizes.at(run_counter % kBenchmarkSizes.size());
     ++run_counter;
@@ -34,18 +33,15 @@ TEST_P(LuchnikovEMultOfDenseMatrixFoxAlgoritmPerfTestTBB, BenchmarkFoxAlgorithm)
 
 namespace {
 
-const auto kPerfTaskList = std::tuple_cat(
-    ppc::util::MakeAllPerfTasks<InType, LuchnikovEMultOfDenseMatrixFoxAlgoritmTBB>(
+const auto kPerfTaskList =
+    std::tuple_cat(ppc::util::MakeAllPerfTasks<InType, LuchnikovEMultOfDenseMatrixFoxAlgoritmTBB>(
         PPC_SETTINGS_luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb));
 
 const auto kGtestParams = ppc::util::TupleToGTestValues(kPerfTaskList);
 const auto kTestPrinter = LuchnikovEMultOfDenseMatrixFoxAlgoritmPerfTestTBB::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(
-    TBBFoxAlgorithmBenchmark,
-    LuchnikovEMultOfDenseMatrixFoxAlgoritmPerfTestTBB,
-    kGtestParams,
-    kTestPrinter);
+INSTANTIATE_TEST_SUITE_P(TBBFoxAlgorithmBenchmark, LuchnikovEMultOfDenseMatrixFoxAlgoritmPerfTestTBB, kGtestParams,
+                         kTestPrinter);
 
 }  // namespace
 }  // namespace luchnikov_e_mult_of_dense_matrices_fox_algorithm_tbb
