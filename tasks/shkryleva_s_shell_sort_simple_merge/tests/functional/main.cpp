6434c6c3fb428b69e2e8c07c1a88b66f10476e42
SvetlanaShkryleva <114089663+SvetlanaShkryleva@users.noreply.github.com>
2026-05-20 09:08:33 +0300

    fix

--- tasks/shkryleva_s_shell_sort_simple_merge/tests/functional/main.cpp
+++ tasks/shkryleva_s_shell_sort_simple_merge/tests/functional/main.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 
-#include <algorithm>
 #include <array>
 #include <cstddef>
 #include <string>
@@ -18,36 +17,12 @@
 
 namespace shkryleva_s_shell_sort_simple_merge {
 
-struct StaticTestCase {
-  std::vector<int> input;
-  std::string name;
-};
-
-const std::array<StaticTestCase, 8> kStaticTestCases = {
-    StaticTestCase{.input = {1, 2, 3, 4, 5, 6}, .name = "sorted_asc"},
-    StaticTestCase{.input = {9, 8, 7, 6, 5, 4, 3, 2, 1}, .name = "sorted_desc"},
-    StaticTestCase{.input = {1}, .name = "single"},
-    StaticTestCase{.input = {2, 2, 2, 2, 2, 2, 2, 2}, .name = "all_equal"},
-    StaticTestCase{.input = {2, 2, 44, 2, 3, 5, 1}, .name = "random_with_duplicates"},
-    StaticTestCase{.input = {2, 1}, .name = "two_unsorted"},
-    StaticTestCase{.input = {1, -2, 3, -5}, .name = "mixed_signs"},
-    StaticTestCase{.input = {1, 22, 13, 51, 2, 1, 2, 2, 34, 41}, .name = "longer_mixed"},
-};
-
 class ShkrylevaSShellMergeFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
- public:
-  static std::string PrintTestParam(const TestType &test_param) {
-    size_t idx = std::get<0>(test_param);
-    return kStaticTestCases.at(idx).name;
-  }
-
  protected:
   void SetUp() override {
-    TestType params = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
-    size_t idx = std::get<0>(params);
-    input_data_ = kStaticTestCases.at(idx).input;
-    expected_data_ = input_data_;
-    std::ranges::sort(expected_data_);
+    TestType param = std::get<static_cast<std::size_t>(ppc::util::GTestParamIndex::kTestParams)>(GetParam());
+    input_data_ = std::get<0>(param);
+    expected_data_ = std::get<1>(param);
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
@@ -56,6 +31,12 @@ class ShkrylevaSShellMergeFuncTests : public ppc::util::BaseRunFuncTests<InType,
 
   InType GetTestInputData() final {
     return input_data_;
+  }
+
+ public:
+  static std::string PrintTestParam(const TestType &param) {
+    const auto &input = std::get<0>(param);
+    return "Size_" + std::to_string(input.size());
   }
 
  private:
@@ -69,13 +50,15 @@ TEST_P(ShkrylevaSShellMergeFuncTests, shellMergeTest) {
   ExecuteTest(GetParam());
 }
 
-const std::array<TestType, 8> kTestParam = []() {
-  std::array<TestType, 8> arr;
-  for (size_t i = 0; i < arr.size(); ++i) {
-    arr[i] = std::make_tuple(i, kStaticTestCases[i].name);
-  }
-  return arr;
-}();
+const std::array<TestType, 8> kTestParam = {
+    TestType{InType{1, 2, 3, 4, 5, 6}, OutType{1, 2, 3, 4, 5, 6}},
+    TestType{InType{9, 8, 7, 6, 5, 4, 3, 2, 1}, OutType{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+    TestType{InType{1}, OutType{1}},
+    TestType{InType{2, 2, 2, 2, 2, 2, 2, 2}, OutType{2, 2, 2, 2, 2, 2, 2, 2}},
+    TestType{InType{2, 2, 44, 2, 3, 5, 1}, OutType{1, 2, 2, 2, 3, 5, 44}},
+    TestType{InType{2, 1}, OutType{1, 2}},
+    TestType{InType{1, -2, 3, -5}, OutType{-5, -2, 1, 3}},
+    TestType{InType{1, 22, 13, 51, 2, 1, 2, 2, 34, 41}, OutType{1, 1, 2, 2, 2, 13, 22, 34, 41, 51}}};
 
 const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<ShkrylevaSShellMergeSEQ, InType>(
                                                kTestParam, PPC_SETTINGS_shkryleva_s_shell_sort_simple_merge),
