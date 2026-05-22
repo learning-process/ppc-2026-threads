53f1624bd51c830baa5e2da253b9cdc5f3ee85ca
Solonin Vlad <mngmnt755@yandex.ru>
2026-05-22 10:39:57 +0300

    restore all tech files for build

---
+++ tasks/solonin_v_radix_sort_batcher/stl/include/ops_stl.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "solonin_v_radix_sort_batcher/common/include/common.hpp"
+#include "task/include/task.hpp"
+#include <cstddef>
+#include <vector>
+
+namespace solonin_v_radix_sort_batcher {
+class RadixSortBatcherSTL : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSTL;
+  }
+  explicit RadixSortBatcherSTL(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  static void SortByDigit(std::vector<int> &data, size_t pos);
+  static void SortBlock(std::vector<int> &data, int lo, int hi);
+  static std::vector<int> OddEvenMerge(const std::vector<int> &a, const std::vector<int> &b);
+};
+}  // namespace solonin_v_radix_sort_batcher
