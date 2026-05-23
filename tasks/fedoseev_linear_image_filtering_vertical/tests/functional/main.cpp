e30db7b54b3ab01b08ed99cab13a7f88f18f2d31
Serge6925 <sergeyfedoseev205@gmail.com>
2026-05-23 14:06:23 +0300

    Fix pre-commit

--- tasks/fedoseev_linear_image_filtering_vertical/tests/functional/main.cpp
+++ tasks/fedoseev_linear_image_filtering_vertical/tests/functional/main.cpp
@@ -69,7 +69,7 @@ void FillGrad(Image &img, [[maybe_unused]] int size) {
 }
 
 void FillRand(Image &img, int size) {
-  unsigned seed = static_cast<unsigned>(size) * 0x9e3779b9u;
+  unsigned seed = static_cast<unsigned>(size) * 0x9e3779b9U;
   std::mt19937 gen(seed);
   std::uniform_int_distribution<int> dist(0, 255);
   for (auto &v : img.data) {
