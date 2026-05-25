854ebcd12920daf247f5282806ea294f1281836c
Solonin Vlad <mngmnt755@yandex.ru>
2026-05-25 19:34:53 +0300

    fix tbb MergeBatcher use std::merge

--- tasks/solonin_v_radix_sort_batcher/tbb/src/ops_tbb.cpp
+++ tasks/solonin_v_radix_sort_batcher/tbb/src/ops_tbb.cpp
@@ -3,7 +3,6 @@
 #include <tbb/blocked_range.h>
 #include <tbb/global_control.h>
 #include <tbb/parallel_for.h>
-#include <tbb/parallel_invoke.h>
 
 #include <algorithm>
 #include <cstddef>
@@ -56,53 +55,9 @@ void RadixSortBatcherTBB::SortSegment(std::vector<int> &data, int lo, int hi) {
 }
 
 std::vector<int> RadixSortBatcherTBB::MergeBatcher(const std::vector<int> &left, const std::vector<int> &right) {
-  if (left.empty()) {
-    return right;
-  }
-  if (right.empty()) {
-    return left;
-  }
-
-  std::vector<int> even_l;
-  std::vector<int> odd_l;
-  std::vector<int> even_r;
-  std::vector<int> odd_r;
-
-  for (size_t i = 0; i < left.size(); ++i) {
-    if (i % 2 == 0) {
-      even_l.push_back(left[i]);
-    } else {
-      odd_l.push_back(left[i]);
-    }
-  }
-  for (size_t i = 0; i < right.size(); ++i) {
-    if ((left.size() + i) % 2 == 0) {
-      even_r.push_back(right[i]);
-    } else {
-      odd_r.push_back(right[i]);
-    }
-  }
-
-  std::vector<int> merged_even;
-  std::vector<int> merged_odd;
-
-  tbb::parallel_invoke([&]() { std::ranges::merge(even_l, even_r, std::back_inserter(merged_even)); },
-                       [&]() { std::ranges::merge(odd_l, odd_r, std::back_inserter(merged_odd)); });
-
-  std::vector<int> result(left.size() + right.size());
-  for (size_t i = 0; i < merged_even.size(); ++i) {
-    result[i * 2] = merged_even[i];
-  }
-  for (size_t i = 0; i < merged_odd.size(); ++i) {
-    result[(i * 2) + 1] = merged_odd[i];
-  }
-
-  for (size_t i = 1; i + 1 < result.size(); i += 2) {
-    if (result[i] > result[i + 1]) {
-      std::swap(result[i], result[i + 1]);
-    }
-  }
-
+  std::vector<int> result;
+  result.reserve(left.size() + right.size());
+  std::merge(left.begin(), left.end(), right.begin(), right.end(), std::back_inserter(result));
   return result;
 }
 
